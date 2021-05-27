9c3b0b6593c5f0f52d4b8be704060037e4c93e06
arminatwork <workwitharmin7@gmail.com>
2021-05-27 22:54:28 +0430

    feat added ticket domain model

---
+++ Pwa.Domain/Product/Ticket.cs
@@ -0,0 +1,34 @@
+﻿using Pwa.Domain.Account;
+using Pwa.Domain.Base;
+using System;
+
+namespace Pwa.Domain.Product
+{
+    public class Ticket : BaseDetail
+    {
+        public int DeveloperId { get; private set; }
+        public string Title { get; private set; }
+        public string Description { get; private set; }
+
+        public Developer Developer { get; private set; }
+
+        protected Ticket()
+        {
+
+        }
+
+        public Ticket(string title, string description, int developerId)
+        {
+            Title = title;
+            Description = description;
+            CreationDate = DateTime.Now;
+            DeveloperId = developerId;
+        }
+
+        public void Edit(string description)
+        {
+            Description = description;
+            LastEditDate = DateTime.Now;
+        }
+    }
+}
