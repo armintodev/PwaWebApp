e67d170dc424b8b263e302b8dfa8729a9d747fa1
arminatwork <workwitharmin7@gmail.com>
2021-05-27 23:09:40 +0430

    feat added statistic configuration model

---
+++ Pwa.Infrastructure/Configuration/StatisticConfiguration.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Pwa.Domain.Account;
+
+namespace Pwa.Infrastructure.EfCore.Configuration
+{
+    public class StatisticConfiguration : IEntityTypeConfiguration<Statistic>
+    {
+        public void Configure(EntityTypeBuilder<Statistic> builder)
+        {
+            builder.ToTable("Statistics");
+            builder.HasKey(_ => _.Id);
+
+            builder.Property(_ => _.Browser)
+                .HasMaxLength(500)
+                .IsRequired();
+
+            builder.Property(_ => _.Device)
+                .HasMaxLength(250);
+
+            builder.Property(_ => _.Os)
+                .HasMaxLength(500)
+                .IsRequired();
+
+            builder.Property(_ => _.Version)
+                .HasMaxLength(500)
+                .IsRequired();
+
+            builder.HasMany(_ => _.Developers)
+                .WithOne(_ => _.Statistic)
+                .HasForeignKey(_ => _.StatisticId);
+        }
+    }
+}
