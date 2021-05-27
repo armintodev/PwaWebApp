e1616a489b28c4526bc0209d1cce920f35f9f554
arminatwork <workwitharmin7@gmail.com>
2021-05-27 23:12:39 +0430

    refactor added apply configuration behavior, db set domain models

--- Pwa.Infrastructure/ApplicationDbContext.cs
+++ Pwa.Infrastructure/ApplicationDbContext.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Pwa.Domain.Account;
+using Pwa.Domain.Product;
+using Pwa.Infrastructure.EfCore.Configuration;
 
 namespace Pwa.Infrastructure.EfCore
 {
@@ -10,5 +12,22 @@ namespace Pwa.Infrastructure.EfCore
         {
 
         }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            var assembly = typeof(UserConfiguration).Assembly;
+            builder.ApplyConfigurationsFromAssembly(assembly);
+        }
+
+        public DbSet<Address> Addresses { get; set; }
+        public DbSet<Developer> Developers { get; set; }
+        public DbSet<Statistic> Statistics { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<SourceSite> SourceSites { get; set; }
+        public DbSet<Ticket> Tickets { get; set; }
+        public DbSet<WebApplication> WebApplications { get; set; }
     }
 }
