dbff0536bea914cb180568786de21d99d0a76712
arminatwork <workwitharmin7@gmail.com>
2021-05-27 23:10:00 +0430

    feat added ticket configuration model

---
+++ Pwa.Infrastructure/Configuration/TicketConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Pwa.Domain.Product;
+
+namespace Pwa.Infrastructure.EfCore.Configuration
+{
+    public class TicketConfiguration : IEntityTypeConfiguration<Ticket>
+    {
+        public void Configure(EntityTypeBuilder<Ticket> builder)
+        {
+            builder.ToTable("Tickets");
+            builder.HasKey(_ => _.Id);
+
+            builder.Property(_ => _.Title)
+                .HasMaxLength(250)
+                .IsRequired();
+
+            builder.Property(_ => _.Description)
+                .IsRequired();
+
+            builder.HasOne(_ => _.Developer)
+                .WithMany(_ => _.Tickets)
+                .HasForeignKey(_ => _.DeveloperId);
+        }
+    }
+}
