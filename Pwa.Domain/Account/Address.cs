baae7bc7e93bb1841e2938786e6a5de3289ce417
arminatwork <workwitharmin7@gmail.com>
2021-05-27 22:52:12 +0430

    feat added address domain model

---
+++ Pwa.Domain/Account/Address.cs
@@ -0,0 +1,26 @@
+﻿using Pwa.Domain.Base;
+using System;
+
+namespace Pwa.Domain.Account
+{
+    public class Address : BaseEntity
+    {
+        public string City { get; private set; }
+        public string Province { get; private set; }
+        public string Country { get; private set; }
+        public DateTime CreationDate { get; private set; }
+
+        protected Address()
+        {
+
+        }
+
+        public Address(string city, string province, string country)
+        {
+            City = city;
+            Province = province;
+            Country = country;
+            CreationDate = DateTime.Now;
+        }
+    }
+}
