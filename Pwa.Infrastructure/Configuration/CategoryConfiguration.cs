50a86efa652960ce025bb65b8228ec05caac2a51
arminatwork <workwitharmin7@gmail.com>
2021-05-27 23:06:15 +0430

    feat added category configuration model

---
+++ Pwa.Infrastructure/Configuration/CategoryConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Pwa.Domain.Product;
+
+namespace Pwa.Infrastructure.EfCore.Configuration
+{
+    public class CategoryConfiguration : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            builder.ToTable("Categories");
+            builder.HasKey(_ => _.Id);
+
+            builder.Property(_ => _.Title)
+                .HasMaxLength(500)
+                .IsRequired();
+
+            builder.HasMany(_ => _.WebApplications)
+               .WithOne(_ => _.Category)
+               .HasForeignKey(_ => _.CategoryId);
+        }
+    }
+}
