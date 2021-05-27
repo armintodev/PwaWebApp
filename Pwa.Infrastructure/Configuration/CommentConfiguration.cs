15996c42d257e1937c485bb8a8191fbd5824014d
arminatwork <workwitharmin7@gmail.com>
2021-05-27 23:06:26 +0430

    feat added comment configuration model

---
+++ Pwa.Infrastructure/Configuration/CommentConfiguration.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Pwa.Domain.Product;
+using System;
+
+namespace Pwa.Infrastructure.EfCore.Configuration
+{
+    class CommentConfiguration : IEntityTypeConfiguration<Comment>
+    {
+        public void Configure(EntityTypeBuilder<Comment> builder)
+        {
+            builder.ToTable("Comments");
+            builder.HasKey(k => k.Id);
+
+            builder.Property(p => p.Description)
+                .HasMaxLength(1500)
+                .IsRequired();
+
+            builder.HasOne(o => o.User)
+                .WithMany(m => m.Comments)
+                .HasForeignKey(f => f.UserId);
+
+            builder.HasOne(o => o.WebApplication)
+                .WithMany(m => m.Comments)
+                .HasForeignKey(f => f.WebApplicationId);
+        }
+    }
+}
