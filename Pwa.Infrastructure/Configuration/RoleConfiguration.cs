f13a6714679ce193f3618fe40b117313e11e80d7
arminatwork <workwitharmin7@gmail.com>
2021-05-27 23:07:45 +0430

    feat added role configuration model

---
+++ Pwa.Infrastructure/Configuration/RoleConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Pwa.Domain.Account;
+
+namespace Pwa.Infrastructure.EfCore.Configuration
+{
+    public class RoleConfiguration : IEntityTypeConfiguration<Role>
+    {
+        public void Configure(EntityTypeBuilder<Role> builder)
+        {
+            builder.ToTable("Roles");
+        }
+    }
+}
