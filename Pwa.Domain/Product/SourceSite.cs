ca1cdb01276c9f9598fb9897c697a86024d9368b
arminatwork <workwitharmin7@gmail.com>
2021-05-27 22:54:16 +0430

    refactor source site domain model

--- Pwa.Domain/Product/SourceSite.cs
+++ Pwa.Domain/Product/SourceSite.cs
@@ -1,22 +1,26 @@
-﻿using Pwa.Domain.Base;
+﻿using Pwa.Domain.Account;
+using Pwa.Domain.Base;
 using System;
 
 namespace Pwa.Domain.Product
 {
     public class SourceSite : BaseDetail
     {
+        public int UserId { get; private set; }
         public string Name { get; private set; }
         public string Description { get; private set; }
         public string Address { get; private set; }
-        public string[] Icons { get; private set; }
+        public byte[] Icons { get; private set; }
         public bool IsPwa { get; set; }
+
+        public User User { get; private set; }
 
         protected SourceSite()
         {
 
         }
 
-        public SourceSite(string name, string description, string address, string[] icons, bool isPwa)
+        public SourceSite(string name, string description, string address, byte[] icons, bool isPwa, int userId)
         {
             Name = name;
             Description = description;
@@ -24,16 +28,17 @@ namespace Pwa.Domain.Product
             Icons = icons;
             IsPwa = isPwa;
             CreationDate = DateTime.Now;
+            UserId = userId;
         }
 
-        public void Edit(string name, string description, string address, string[] icons, bool isPwa)
+        public void Edit(string name, string description, string address, byte[] icons, bool isPwa)
         {
             Name = name;
             Description = description;
             Address = address;
             Icons = icons;
             IsPwa = isPwa;
-            LastEditdate = DateTime.Now;
+            LastEditDate = DateTime.Now;
         }
     }
 }
