b836ee1ad5c801ff7b424de4bb3c754b651617cf
arminatwork <workwitharmin7@gmail.com>
2021-05-27 23:10:51 +0430

    feat added user configuration model

---
+++ Pwa.Infrastructure/Configuration/UserConfiguration.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Pwa.Domain.Account;
+
+namespace Pwa.Infrastructure.EfCore.Configuration
+{
+    public class UserConfiguration : IEntityTypeConfiguration<User>
+    {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            builder.ToTable("Users");
+            builder.HasKey(_ => _.Id);
+
+            builder.Property(_ => _.PhoneNumber)
+                .HasMaxLength(20)
+                .IsRequired();
+
+            builder.Property(_ => _.Email)
+                .HasMaxLength(500)
+                .IsRequired();
+
+            builder.Property(_ => _.UserName)
+                .HasMaxLength(500)
+                .IsRequired();
+
+            builder.Property(_ => _.FullName)
+                .HasMaxLength(500)
+                .IsRequired();
+
+            builder.Property(_ => _.Status)
+                .IsRequired();
+
+            builder.HasMany(_ => _.Comments)
+                .WithOne(_ => _.User)
+                .HasForeignKey(_ => _.UserId);
+        }
+    }
+}
