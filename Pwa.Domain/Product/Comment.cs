98f270d6bfab8651849923efa00d40bd1d9031dc
arminatwork <workwitharmin7@gmail.com>
2021-05-27 22:53:48 +0430

    feat added comment domain model

---
+++ Pwa.Domain/Product/Comment.cs
@@ -0,0 +1,50 @@
+﻿using Pwa.Domain.Account;
+using Pwa.Domain.Base;
+using System;
+using WebFramework.Enums;
+
+namespace Pwa.Domain.Product
+{
+    public class Comment : BaseDetail
+    {
+        public int UserId { get; private set; }
+        public int WebApplicationId { get; private set; }
+        public string Description { get; private set; }
+        public bool IsDeveloper { get; private set; }
+        public Status Status { get; private set; }
+
+        public User User { get; private set; }
+        public WebApplication WebApplication { get; private set; }
+
+        protected Comment()
+        {
+
+        }
+
+        public Comment(string description, bool isDeveloper, int userId, int webApplicationId)
+        {
+            Description = description;
+            IsDeveloper = isDeveloper;
+            Status = Status.Pending;
+            UserId = userId;
+            WebApplicationId = webApplicationId;
+            CreationDate = DateTime.Now;
+        }
+
+        public void Edit(string description)
+        {
+            Description = description;
+            LastEditDate = DateTime.Now;
+        }
+
+        public void Accept()
+        {
+            Status = Status.Accepted;
+        }
+
+        public void Reject()
+        {
+            Status = Status.Rejected;
+        }
+    }
+}
