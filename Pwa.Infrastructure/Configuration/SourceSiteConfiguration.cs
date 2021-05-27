8cf43d4fff7f84cbcb4aa1cf6d6745af8beffe22
arminatwork <workwitharmin7@gmail.com>
2021-05-27 23:08:42 +0430

    feat added source site configuration model

---
+++ Pwa.Infrastructure/Configuration/SourceSiteConfiguration.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Pwa.Domain.Product;
+
+namespace Pwa.Infrastructure.EfCore.Configuration
+{
+    public class SourceSiteConfiguration : IEntityTypeConfiguration<SourceSite>
+    {
+        public void Configure(EntityTypeBuilder<SourceSite> builder)
+        {
+            builder.ToTable("SourceSites");
+            builder.HasKey(_ => _.Id);
+
+            builder.Property(_ => _.Name)
+                 .HasMaxLength(250)
+                 .IsRequired();
+
+            builder.Property(_ => _.Description)
+                .HasMaxLength(5000)
+                .IsRequired();
+            builder.Property(_ => _.Address)
+                .HasMaxLength(500)
+                .IsRequired();
+
+            builder.Property(_ => _.Icons)
+                .HasMaxLength(1000)
+                .IsRequired();
+
+            builder.HasOne(_ => _.User)
+                .WithMany(_ => _.SourceSites)
+                .HasForeignKey(_ => _.UserId);
+        }
+    }
+}
