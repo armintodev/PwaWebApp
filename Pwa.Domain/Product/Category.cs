6a672b486e44220b6d1ae1da25d4b77d7e55b66c
arminatwork <workwitharmin7@gmail.com>
2021-05-27 22:53:37 +0430

    feat added category domain model

---
+++ Pwa.Domain/Product/Category.cs
@@ -0,0 +1,31 @@
+﻿using Pwa.Domain.Base;
+using System;
+using System.Collections.Generic;
+
+namespace Pwa.Domain.Product
+{
+    public class Category : BaseDetail
+    {
+        public string Title { get; private set; }
+
+        public ICollection<WebApplication> WebApplications { get; private set; }
+
+        protected Category()
+        {
+
+        }
+
+        public Category(string title)
+        {
+            Title = title;
+            WebApplications = new List<WebApplication>();
+            CreationDate = DateTime.Now;
+        }
+
+        public void Edit(string title)
+        {
+            Title = title;
+            LastEditDate = DateTime.Now;
+        }
+    }
+}
