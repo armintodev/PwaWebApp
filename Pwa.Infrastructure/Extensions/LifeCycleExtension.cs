bf66502a955a906280820e1c4f68a6ed4e0d3a46
arminatwork <workwitharmin7@gmail.com>
2021-05-28 21:45:12 +0430

    feat added life cycle extension

---
+++ Pwa.Infrastructure/Extensions/LifeCycleExtension.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Pwa.Domain.Account;
+using Pwa.Domain.Product;
+using Pwa.Infrastructure.EfCore.Repositories;
+
+namespace Pwa.Infrastructure.EfCore.Extensions
+{
+    internal static class LifeCycleExtension
+    {
+        public static void AddLifeCycleApplication(this IServiceCollection service)
+        {
+            service.AddScoped<IAddressRepository, AddressRepository>();
+            service.AddScoped<IDeveloperRepository, DeveloperRepository>();
+            service.AddScoped<IRoleRepository, RoleRepository>();
+            service.AddScoped<IStatisticRepository, StatisticRepository>();
+            service.AddScoped<IUserRepository, UserRepository>();
+            service.AddScoped<ICategoryRepository, CategoryRepository>();
+            service.AddScoped<ICommentRepository, CommentRepository>();
+            service.AddScoped<ISourceSiteRepository, SourceSiteRepository>();
+            service.AddScoped<ITicketRepository, TicketRepository>();
+            service.AddScoped<IWebApplicationRepository, WebApplicationRepository>();
+        }
+    }
+}
