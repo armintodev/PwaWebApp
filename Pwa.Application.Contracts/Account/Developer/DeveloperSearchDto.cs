c39f286161cc0c6a06255052b2c1be30de7e3519
arminatwork <workwitharmin7@gmail.com>
2021-05-29 16:38:12 +0430

    feat added developer search dto model

---
+++ Pwa.Application.Contracts/Account/Developer/DeveloperSearchDto.cs
@@ -0,0 +1,13 @@
+﻿using WebFramework.Enums;
+using WebFramework.Infrastructure;
+
+namespace Pwa.Application.Contracts.Account.Developer
+{
+    public record DeveloperSearchDto : IDto
+    {
+        public string NationalCode { get; init; }
+        public string Email { get; init; }
+        public string PhoneNumber { get; init; }
+        public StatusDto Status { get; init; }
+    }
+}
