6dc93b779d96284149f5d2fc1169cf9410456c39
arminatwork <workwitharmin7@gmail.com>
2021-05-29 16:27:01 +0430

    feat added status dto

---
+++ WebFramework/Enums/StatusDto.cs
@@ -0,0 +1,11 @@
+﻿namespace WebFramework.Enums
+{
+    public enum StatusDto
+    {
+        Active = 0,
+        DeActive = 1,
+        Accepted = 10,
+        Rejected = 11,
+        Pending = 12
+    }
+}
