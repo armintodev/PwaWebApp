c6c702a31b7b19fd0df413fdfc1fbda52a2163f5
arminatwork <workwitharmin7@gmail.com>
2021-05-29 16:38:21 +0430

    feat added edit developer dto model

---
+++ Pwa.Application.Contracts/Account/Developer/EditDeveloperDto.cs
@@ -0,0 +1,9 @@
+﻿using WebFramework.Infrastructure;
+
+namespace Pwa.Application.Contracts.Account.Developer
+{
+    public record EditDeveloperDto : CreateDeveloperDto, IDto
+    {
+        public int Id { get; init; }
+    }
+}
