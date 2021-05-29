a58ac744385b7f6e582d1f13bb4e99fb0eace91b
arminatwork <workwitharmin7@gmail.com>
2021-05-29 16:37:43 +0430

    feat added create developer dto model

---
+++ Pwa.Application.Contracts/Account/Developer/CreateDeveloperDto.cs
@@ -0,0 +1,14 @@
+﻿using WebFramework.Infrastructure;
+
+namespace Pwa.Application.Contracts.Account.Developer
+{
+    public record CreateDeveloperDto : IDto
+    {
+        public string FullName { get; init; }
+        public string Email { get; init; }
+        public string PhoneNumber { get; init; }
+        public string NationalCode { get; init; }
+        public int AddressId { get; init; }
+        public int StatisticId { get; init; }
+    }
+}
