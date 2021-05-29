ffb488718e0a18de4965e070add58bee57fe6e50
arminatwork <workwitharmin7@gmail.com>
2021-05-29 17:09:50 +0430

    refactor inject developer respository

--- Pwa.Application/DeveloperApplication.cs
+++ Pwa.Application/DeveloperApplication.cs
@@ -1,9 +1,14 @@
 ﻿using Pwa.Application.Contracts.Account.Developer;
+using Pwa.Domain.Account;
 
 namespace Pwa.Application
 {
     public class DeveloperApplication : IDeveloperApplication<DeveloperDto>
     {
-
+        private readonly IDeveloperRepository _developer;
+        public DeveloperApplication(IDeveloperRepository developer)
+        {
+            _developer = developer;
+        }
     }
 }
