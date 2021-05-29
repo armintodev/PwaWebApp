8410c8c581a4e852385040b53c09c1e36b30fc6d
arminatwork <workwitharmin7@gmail.com>
2021-05-29 17:47:01 +0430

    feat added developer expression to dto

---
+++ Pwa.Infrastructure/Expressions/DeveloperExpression.cs
@@ -0,0 +1,24 @@
+﻿using Pwa.Application.Contracts.Account.Developer;
+using Pwa.Domain.Account;
+using System;
+using System.Linq.Expressions;
+using WebFramework.Enums;
+
+namespace Pwa.Infrastructure.EfCore.Expressions
+{
+    public static class DeveloperExpression
+    {
+        public static Expression<Func<Developer, DeveloperDto>> ToDto => developer => new DeveloperDto
+        {
+            Id = developer.Id,
+            FullName = developer.FullName,
+            Email = developer.Email,
+            PhoneNumber = developer.PhoneNumber,
+            NationalCode = developer.NationalCode,
+            Status = (StatusDto)developer.Status,
+            CreationDate = developer.CreationDate.ToString(),
+            AddressId = developer.AddressId,
+            StatisticId = developer.StatisticId
+        };
+    }
+}
