58073cb6bbd7a42b19bbc953e00aecdc448d7683
arminatwork <workwitharmin7@gmail.com>
2021-05-29 16:37:54 +0430

    feat added developer dto model

---
+++ Pwa.Application.Contracts/Account/Developer/DeveloperDto.cs
@@ -0,0 +1,18 @@
+﻿using WebFramework.Enums;
+using WebFramework.Infrastructure;
+
+namespace Pwa.Application.Contracts.Account.Developer
+{
+    public record DeveloperDto : IDto
+    {
+        public int Id { get; init; }
+        public string FullName { get; init; }
+        public string NationalCode { get; init; }
+        public string Email { get; init; }
+        public string PhoneNumber { get; init; }
+        public StatusDto Status { get; init; }
+        public string CreationDate { get; init; }
+        public int AddressId { get; init; }
+        public int StatisticId { get; init; }
+    }
+}
