9c3deabecff40eb79d0d111ed701eea95927e8de
arminatwork <workwitharmin7@gmail.com>
2021-05-31 22:30:51 +0430

    feat added user data transfer object

---
+++ Pwa.Application.Contracts/Account/User/UserDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using WebFramework.Enums;
+using WebFramework.Infrastructure;
+
+namespace Pwa.Application.Contracts.Account.User
+{
+    public record UserDto : IDto
+    {
+        public int Id { get; init; }
+        public string FullName { get; init; }
+        public string Email { get; init; }
+        public string PhoneNumber { get; init; }
+        public string Code { get; init; }
+        public StatusDto Status { get; init; }
+        public string CreationDate { get; init; }
+    }
+}
