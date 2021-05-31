78f9b83e086e5f9eef154acd39a550c9d57afedf
arminatwork <workwitharmin7@gmail.com>
2021-05-31 23:25:21 +0430

    feat added validation messages constant

---
+++ WebFramework/Utilities/ValidationMessages.cs
@@ -0,0 +1,10 @@
+﻿namespace WebFramework.Utilities
+{
+    public record ValidationMessages
+    {
+        public const string Required = "مقدار {0} نباید خالی باشد.";
+        public const string CheckPassword = "پسورد ها مطابقت ندارند";
+        public const string WrongPhoneNumber = "شماره همراه درست وارد کنید.";
+        public const string WrongMaxLength = "مقدار {0} نمیتواند از {1} بیشتر باشد.";
+    }
+}
