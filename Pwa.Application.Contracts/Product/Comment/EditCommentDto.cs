e5bf4e815c8eb864a630e30e63c3c20d7b5a223a
arminatwork <workwitharmin7@gmail.com>
2021-05-31 22:33:05 +0430

    feat added edit comment data transfer object

---
+++ Pwa.Application.Contracts/Product/Comment/EditCommentDto.cs
@@ -0,0 +1,9 @@
+﻿using WebFramework.Infrastructure;
+
+namespace Pwa.Application.Contracts.Product.Comment
+{
+    public record EditCommentDto : CreateCommentDto, IDto
+    {
+        public int Id { get; init; }
+    }
+}
