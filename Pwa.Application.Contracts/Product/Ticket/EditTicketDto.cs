2c0ea52f8e2f0da1e48deddc962eaafa0a0ac90a
arminatwork <workwitharmin7@gmail.com>
2021-05-31 22:43:49 +0430

    feat added edit ticket data transfer object

---
+++ Pwa.Application.Contracts/Product/Ticket/EditTicketDto.cs
@@ -0,0 +1,9 @@
+﻿using WebFramework.Infrastructure;
+
+namespace Pwa.Application.Contracts.Product.Ticket
+{
+    public record EditTicketDto : IDto
+    {
+        public int Id { get; init; }
+    }
+}
