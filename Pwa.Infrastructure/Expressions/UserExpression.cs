81995181678dc5b3e8ed66d9a8b6811de14cd20b
arminatwork <workwitharmin7@gmail.com>
2021-05-31 23:21:55 +0430

    feat added user expression

---
+++ Pwa.Infrastructure/Expressions/UserExpression.cs
@@ -0,0 +1,22 @@
+﻿using Pwa.Application.Contracts.Account.User;
+using Pwa.Domain.Account;
+using System;
+using System.Linq.Expressions;
+using WebFramework.Enums;
+
+namespace Pwa.Infrastructure.EfCore.Expressions
+{
+    public static class UserExpression
+    {
+        public static Expression<Func<User, UserDto>> ToDto => _ => new UserDto
+        {
+            Id = _.Id,
+            FullName = _.FullName,
+            Email = _.Email,
+            PhoneNumber = _.PhoneNumber,
+            Code = _.Code,
+            Status = (StatusDto)_.Status,
+            CreationDate = _.CreationDate.ToString(),
+        };
+    }
+}
