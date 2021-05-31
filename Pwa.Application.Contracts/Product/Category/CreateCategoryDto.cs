cc35e45d98a809b46a1d448999d764846d0e3562
arminatwork <workwitharmin7@gmail.com>
2021-05-31 22:32:11 +0430

    feat added create category data transfer object

---
+++ Pwa.Application.Contracts/Product/Category/CreateCategoryDto.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+using WebFramework.Infrastructure;
+using WebFramework.Utilities;
+
+namespace Pwa.Application.Contracts.Product.Category
+{
+    public record CreateCategoryDto : IDto
+    {
+        [Display(Name = "عنوان")]
+        [Required(ErrorMessage = ValidationMessages.Required)]
+        public string Title { get; init; }
+    }
+}
