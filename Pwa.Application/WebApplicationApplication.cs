207a3730d20dff219583b0b765d38c71b1dfbc47
arminatwork <workwitharmin7@gmail.com>
2021-05-31 22:22:15 +0430

    feat added 'web application' application implementation

---
+++ Pwa.Application/WebApplicationApplication.cs
@@ -0,0 +1,14 @@
+﻿using Pwa.Application.Contracts.Product.WebApplication;
+using Pwa.Domain.Product;
+
+namespace Pwa.Application
+{
+    public class WebApplicationApplication : IWebApplicationApplication
+    {
+        private readonly IWebApplicationRepository _webApplication;
+        public WebApplicationApplication(IWebApplicationRepository webApplication)
+        {
+            _webApplication = webApplication;
+        }
+    }
+}
