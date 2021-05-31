9629489d27a12d60ce17578a813d7661f53dbed9
arminatwork <workwitharmin7@gmail.com>
2021-05-31 22:26:48 +0430

    refactor added data annotation on properties,added code property

--- Pwa.Application.Contracts/Account/Developer/CreateDeveloperDto.cs
+++ Pwa.Application.Contracts/Account/Developer/CreateDeveloperDto.cs
@@ -1,13 +1,37 @@
-﻿using WebFramework.Infrastructure;
+﻿using System.ComponentModel.DataAnnotations;
+using WebFramework.Infrastructure;
+using WebFramework.Utilities;
 
 namespace Pwa.Application.Contracts.Account.Developer
 {
     public record CreateDeveloperDto : IDto
     {
+        [Display(Name = "نام کامل")]
+        [Required(ErrorMessage = ValidationMessages.Required)]
         public string FullName { get; init; }
+
+        [Display(Name = "ایمیل")]
+        [Required(ErrorMessage = ValidationMessages.Required)]
         public string Email { get; init; }
+
+        [Display(Name = "شماره همراه")]
+        [Required(ErrorMessage = ValidationMessages.Required)]
         public string PhoneNumber { get; init; }
+
+        [Display(Name = "کلمه عبور")]
+        [Required(ErrorMessage = ValidationMessages.Required)]
+        public string Password { get; init; }
+
+        [Display(Name = "تایید کلمه عبور")]
+        [Required(ErrorMessage = ValidationMessages.Required)]
+        [Compare(nameof(Password), ErrorMessage = ValidationMessages.CheckPassword)]
+        public string RePassword { get; init; }
+
+        [Display(Name = "کدملی")]
+        [Required(ErrorMessage = ValidationMessages.Required)]
         public string NationalCode { get; init; }
+        public string Code { get; init; }
+
         public int AddressId { get; init; }
         public int StatisticId { get; init; }
     }
