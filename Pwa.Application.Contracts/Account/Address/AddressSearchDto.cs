c0294143d0996276e2f5a8a42a19e94cd016b98c
arminatwork <workwitharmin7@gmail.com>
2021-05-31 22:23:19 +0430

    feat added address search data transfer object

---
+++ Pwa.Application.Contracts/Account/Address/AddressSearchDto.cs
@@ -0,0 +1,11 @@
+﻿using WebFramework.Infrastructure;
+
+namespace Pwa.Application.Contracts.Account.Address
+{
+    public record AddressSearchDto : IDto
+    {
+        public string City { get; init; }
+        public string Province { get; init; }
+        public string Country { get; init; }
+    }
+}
