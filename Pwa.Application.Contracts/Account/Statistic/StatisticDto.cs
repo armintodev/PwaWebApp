7871134e6d5e422bfdf8df536f8d4357fe3bed1b
arminatwork <workwitharmin7@gmail.com>
2021-05-31 22:28:52 +0430

    feat added statistic data transfer object

---
+++ Pwa.Application.Contracts/Account/Statistic/StatisticDto.cs
@@ -0,0 +1,15 @@
+﻿using WebFramework.Infrastructure;
+
+namespace Pwa.Application.Contracts.Account.Statistic
+{
+    public record StatisticDto : IDto
+    {
+        public int Id { get; init; }
+        public string Browser { get; init; }
+        public string IpAddress { get; init; }
+        public string Device { get; init; }
+        public string Os { get; init; }
+        public string Version { get; init; }
+        public string CreationDate { get; init; }
+    }
+}
