78611cbfb156bb445a04fa6282e0415919bf502a
arminatwork <workwitharmin7@gmail.com>
2021-05-31 23:14:03 +0430

    refactor added get by email signature

--- Pwa.Domain/Account/IDeveloperRepository.cs
+++ Pwa.Domain/Account/IDeveloperRepository.cs
@@ -1,8 +1,10 @@
-﻿using WebFramework.Domain;
+﻿using System.Threading.Tasks;
+using WebFramework.Domain;
 
 namespace Pwa.Domain.Account
 {
     public interface IDeveloperRepository : IBaseRepository<Developer>
     {
+        Task<Developer> GetByEmail(string email);
     }
 }
