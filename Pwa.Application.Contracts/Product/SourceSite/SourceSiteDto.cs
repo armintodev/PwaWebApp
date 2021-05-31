da6bfe783e744d77df8c83ca8ad8890328ad873f
arminatwork <workwitharmin7@gmail.com>
2021-06-01 00:32:27 +0430

    refactor added description property

--- Pwa.Application.Contracts/Product/SourceSite/SourceSiteDto.cs
+++ Pwa.Application.Contracts/Product/SourceSite/SourceSiteDto.cs
@@ -7,11 +7,11 @@ namespace Pwa.Application.Contracts.Product.SourceSite
     {
         public int Id { get; init; }
         public string Name { get; init; }
+        public string Description { get; init; }
         public string Address { get; init; }
         public bool IsPwa { get; init; }
         public int UserId { get; init; }
         public string UserFullName { get; init; }
         public string UserPhoneNumber { get; init; }
-        //public UserDto User { get; init; }
     }
 }
