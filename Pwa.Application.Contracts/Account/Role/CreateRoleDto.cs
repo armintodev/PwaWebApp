eb0a3ff81c0261d50e121e4df92d112f9cf9fc72
arminatwork <workwitharmin7@gmail.com>
2021-05-31 22:27:44 +0430

    feat added create role data transfer object

---
+++ Pwa.Application.Contracts/Account/Role/CreateRoleDto.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+using WebFramework.Infrastructure;
+using WebFramework.Utilities;
+
+namespace Pwa.Application.Contracts.Account.Role
+{
+    public record CreateRoleDto : IDto
+    {
+        [Display(Name = "نام")]
+        [Required(ErrorMessage = ValidationMessages.Required)]
+        public string Name { get; init; }
+    }
+}
