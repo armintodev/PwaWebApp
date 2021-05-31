2d4bb472eee420ecfb446987777d4b0399ae0a6d
arminatwork <workwitharmin7@gmail.com>
2021-05-31 22:44:19 +0430

    feat added ticket data transfer object

---
+++ Pwa.Application.Contracts/Product/Ticket/TicketDto.cs
@@ -0,0 +1,14 @@
+﻿using WebFramework.Infrastructure;
+
+namespace Pwa.Application.Contracts.Product.Ticket
+{
+    public record TicketDto : IDto
+    {
+        public int Id { get; init; }
+        public string Title { get; init; }
+        public string Description { get; init; }
+        public int DeveloperId { get; init; }
+        public string DeveloperFullName { get; init; }
+        public string DeveloperEmail { get; init; }
+    }
+}
