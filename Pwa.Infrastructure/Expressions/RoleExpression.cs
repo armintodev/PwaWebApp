ef780eb79141c27e90673ecb1821bbe18a43f414
arminatwork <workwitharmin7@gmail.com>
2021-05-31 23:21:14 +0430

    feat added role expression

---
+++ Pwa.Infrastructure/Expressions/RoleExpression.cs
@@ -0,0 +1,16 @@
+﻿using Pwa.Application.Contracts.Account.Role;
+using Pwa.Domain.Account;
+using System;
+using System.Linq.Expressions;
+
+namespace Pwa.Infrastructure.EfCore.Expressions
+{
+    public static class RoleExpression
+    {
+        public static Expression<Func<Role, RoleDto>> ToDto => _ => new RoleDto
+        {
+            Id = _.Id,
+            Name = _.Name
+        };
+    }
+}
