a1a3f6a2e191bd0260e4bea1251de655587b6f2a
arminatwork <workwitharmin7@gmail.com>
2021-05-31 22:28:17 +0430

    feat added role search data transfer object

---
+++ Pwa.Application.Contracts/Account/Role/RoleSearchDto.cs
@@ -0,0 +1,9 @@
+﻿using WebFramework.Infrastructure;
+
+namespace Pwa.Application.Contracts.Account.Role
+{
+    public record RoleSearchDto : IDto
+    {
+        public string Name { get; init; }
+    }
+}
