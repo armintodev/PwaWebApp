2cda65bff8a5bd5cb44cb3bff4a602bcdb4bad6e
arminatwork <workwitharmin7@gmail.com>
2021-05-31 23:22:10 +0430

    feat added web application expression

---
+++ Pwa.Infrastructure/Expressions/WebApplicationExpression.cs
@@ -0,0 +1,26 @@
+﻿using Pwa.Application.Contracts.Product.WebApplication;
+using Pwa.Domain.Product;
+using System;
+using System.Linq.Expressions;
+using WebFramework.Enums;
+
+namespace Pwa.Infrastructure.EfCore.Expressions
+{
+    public static class WebApplicationExpression
+    {
+        public static Expression<Func<WebApplication, WebApplicationDto>> ToDto => _ => new WebApplicationDto
+        {
+            Id = _.Id,
+            Name = _.Name,
+            Description = _.Description,
+            WebSiteAddress = _.WebSiteAddress,
+            Installed = _.Installed,
+            Visit = _.Visit,
+            Icons = _.Icons,
+            Status = (StatusDto)_.Status,
+            TypeAdd = (TypeAddDto)_.TypeAdd,
+            CategoryId = _.CategoryId,
+            DeveloperId = _.DeveloperId,
+        };
+    }
+}
