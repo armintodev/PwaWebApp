262ea41e0110516cce5069ade66ec5c15fd65c55
arminatwork <workwitharmin7@gmail.com>
2021-05-31 22:31:00 +0430

    feat added user search data transfer object

---
+++ Pwa.Application.Contracts/Account/User/UserSearchDto.cs
@@ -0,0 +1,13 @@
+﻿using WebFramework.Enums;
+using WebFramework.Infrastructure;
+
+namespace Pwa.Application.Contracts.Account.User
+{
+    public record UserSearchDto : IDto
+    {
+        public string PhoneNumber { get; init; }
+        public string Email { get; set; }
+        public string FullName { get; init; }
+        public StatusDto Status { get; init; }
+    }
+}
