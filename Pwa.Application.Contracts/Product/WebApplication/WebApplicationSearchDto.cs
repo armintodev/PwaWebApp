07db7d4fa5e5a3485c1bfa4842f058562cc63fba
arminatwork <workwitharmin7@gmail.com>
2021-05-31 22:45:19 +0430

    feat added web application search data transfer object

---
+++ Pwa.Application.Contracts/Product/WebApplication/WebApplicationSearchDto.cs
@@ -0,0 +1,15 @@
+﻿using WebFramework.Enums;
+using WebFramework.Infrastructure;
+
+namespace Pwa.Application.Contracts.Product.WebApplication
+{
+    public record WebApplicationSearchDto : IDto
+    {
+        public string Name { get; init; }
+        public string WebSiteAddress { get; init; }
+        public TypeAddDto TypeAdd { get; init; }
+        public StatusDto Status { get; init; }
+        public int Visit { get; init; }
+        public int Installed { get; init; }
+    }
+}
