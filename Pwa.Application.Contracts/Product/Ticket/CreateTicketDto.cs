4ed95374d98fbd4b21261a8a77c4271a3abf35ea
arminatwork <workwitharmin7@gmail.com>
2021-05-31 22:35:49 +0430

    feat added create ticket data transfer object

---
+++ Pwa.Application.Contracts/Product/Ticket/CreateTicketDto.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using WebFramework.Infrastructure;
+using WebFramework.Utilities;
+
+namespace Pwa.Application.Contracts.Product.Ticket
+{
+    public record CreateTicketDto : IDto
+    {
+        [Display(Name = "عنوان")]
+        [Required(ErrorMessage = ValidationMessages.Required)]
+        [MaxLength(500, ErrorMessage = ValidationMessages.WrongMaxLength)]
+        public string Title { get; init; }
+
+        [Display(Name = "توضیح")]
+        [Required(ErrorMessage = ValidationMessages.Required)]
+        public string Description { get; init; }
+
+        public int DeveloperId { get; init; }
+    }
+}
