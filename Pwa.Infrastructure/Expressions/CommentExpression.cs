0d3ba6cded4058e4e3e946ebd638b3b9c2cd5647
arminatwork <workwitharmin7@gmail.com>
2021-05-31 23:20:19 +0430

    feat added comment expression

---
+++ Pwa.Infrastructure/Expressions/CommentExpression.cs
@@ -0,0 +1,25 @@
+﻿using Pwa.Application.Contracts.Product.Comment;
+using Pwa.Domain.Product;
+using System;
+using System.Linq.Expressions;
+using WebFramework.Enums;
+
+namespace Pwa.Infrastructure.EfCore.Expressions
+{
+    public static class CommentExpression
+    {
+        public static Expression<Func<Comment, CommentDto>> ToDto => _ => new CommentDto
+        {
+            Id = _.Id,
+            Description = _.Description,
+            IsDeveloper = _.IsDeveloper,
+            Status = (StatusDto)_.Status,
+            UserId = _.UserId,
+            WebApplicationId = _.WebApplicationId,
+            UserFullName = _.User.FullName,
+            UserPhoneNumber = _.User.PhoneNumber,
+            WebAppName = _.WebApplication.Name,
+            WebAppAddress = _.WebApplication.WebSiteAddress
+        };
+    }
+}
