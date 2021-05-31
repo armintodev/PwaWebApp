40552e96208ab5db0aa7a4c43eba626942aec74b
arminatwork <workwitharmin7@gmail.com>
2021-05-31 22:16:10 +0430

    feat added comment application implementation

---
+++ Pwa.Application/CommentApplication.cs
@@ -0,0 +1,14 @@
+﻿using Pwa.Application.Contracts.Product.Comment;
+using Pwa.Domain.Product;
+
+namespace Pwa.Application
+{
+    public class CommentApplication : ICommentApplication
+    {
+        private readonly ICommentRepository _comment;
+        public CommentApplication(ICommentRepository comment)
+        {
+            _comment = comment;
+        }
+    }
+}
