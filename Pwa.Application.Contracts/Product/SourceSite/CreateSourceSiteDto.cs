b1d0825ab8c4a34cb780662454341214d9bf31b4
arminatwork <workwitharmin7@gmail.com>
2021-05-31 22:33:38 +0430

    feat added create source site data transfer object

---
+++ Pwa.Application.Contracts/Product/SourceSite/CreateSourceSiteDto.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+using WebFramework.Infrastructure;
+using WebFramework.Utilities;
+
+namespace Pwa.Application.Contracts.Product.SourceSite
+{
+    public record CreateSourceSiteDto : IDto
+    {
+        [Display(Name = "نام")]
+        [Required(ErrorMessage = ValidationMessages.Required)]
+        [MaxLength(500,ErrorMessage =ValidationMessages.WrongMaxLength)]
+        public string Name { get; init; }
+
+        [Display(Name = "آدرس")]
+        [Required(ErrorMessage = ValidationMessages.Required)]
+        public string Address { get; init; }
+
+        [Display(Name = "توشیح")]
+        [Required(ErrorMessage = ValidationMessages.Required)]
+        public string Description { get; init; }
+
+        [Display(Name = "آیکون")]
+        [Required(ErrorMessage = ValidationMessages.Required)]
+        public byte[] Icons { get; init; }
+
+        [Display(Name = "تایید کردن سایت")]
+        public bool IsPwa { get; init; }
+        public int UserId { get; init; }
+    }
+}
