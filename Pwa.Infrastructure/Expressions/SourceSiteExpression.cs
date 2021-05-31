4ddbe8e225847fae3adf3f534027c63f1f8260b8
arminatwork <workwitharmin7@gmail.com>
2021-05-31 23:21:26 +0430

    feat added source site expression

---
+++ Pwa.Infrastructure/Expressions/SourceSiteExpression.cs
@@ -0,0 +1,21 @@
+﻿using Pwa.Application.Contracts.Product.SourceSite;
+using Pwa.Domain.Product;
+using System;
+using System.Linq.Expressions;
+
+namespace Pwa.Infrastructure.EfCore.Expressions
+{
+    public static class SourceSiteExpression
+    {
+        public static Expression<Func<SourceSite, SourceSiteDto>> ToDto => _ => new SourceSiteDto
+        {
+            Id = _.Id,
+            Name = _.Name,
+            Address = _.Address,
+            IsPwa = _.IsPwa,
+            UserId = _.UserId,
+            UserFullName = _.User.FullName,
+            UserPhoneNumber = _.User.PhoneNumber
+        };
+    }
+}
