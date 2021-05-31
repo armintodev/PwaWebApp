0539f8932608681e88937a50c78eac2280a251c3
arminatwork <workwitharmin7@gmail.com>
2021-05-31 22:34:43 +0430

    feat added source site search data transfer object

---
+++ Pwa.Application.Contracts/Product/SourceSite/SourceSiteSearchDto.cs
@@ -0,0 +1,11 @@
+﻿using WebFramework.Infrastructure;
+
+namespace Pwa.Application.Contracts.Product.SourceSite
+{
+    public record SourceSiteSearchDto : IDto
+    {
+        public string Name { get; init; }
+        public string Address { get; init; }
+        public string Description { get; init; }
+    }
+}
