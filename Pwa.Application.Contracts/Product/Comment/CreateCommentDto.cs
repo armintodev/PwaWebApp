53cc7cf67edb1448e1c07ac087740657f2668c9a
arminatwork <workwitharmin7@gmail.com>
2021-05-31 22:32:54 +0430

    feat added create comment data transfer object

---
+++ Pwa.Application.Contracts/Product/Comment/CreateCommentDto.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using WebFramework.Infrastructure;
+using WebFramework.Utilities;
+
+namespace Pwa.Application.Contracts.Product.Comment
+{
+    public record CreateCommentDto : IDto
+    {
+        [Display(Name = "توضیح")]
+        [Required(ErrorMessage = ValidationMessages.Required)]
+        public string Description { get; init; }
+        public int UserId { get; init; }
+        public int WebApplicationId { get; init; }
+    }
+}
