c1360bf1d99d6ea0c1a889c62dc371a98c0bc6f2
arminatwork <workwitharmin7@gmail.com>
2021-05-31 23:21:41 +0430

    feat added ticket expression

---
+++ Pwa.Infrastructure/Expressions/TicketExpression.cs
@@ -0,0 +1,20 @@
+﻿using Pwa.Application.Contracts.Product.Ticket;
+using Pwa.Domain.Product;
+using System;
+using System.Linq.Expressions;
+
+namespace Pwa.Infrastructure.EfCore.Expressions
+{
+    public static class TicketExpression
+    {
+        public static Expression<Func<Ticket, TicketDto>> ToDto => _ => new TicketDto
+        {
+            Id = _.Id,
+            Title = _.Title,
+            Description = _.Description,
+            DeveloperId = _.DeveloperId,
+            DeveloperFullName = _.Developer.FullName,
+            DeveloperEmail = _.Developer.Email
+        };
+    }
+}
