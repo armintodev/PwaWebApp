17cb23ede14946c0b0742181b6f6aed690e755f5
arminatwork <workwitharmin7@gmail.com>
2021-05-31 22:27:51 +0430

    feat added edit role data transfer object

---
+++ Pwa.Application.Contracts/Account/Role/EditRoleDto.cs
@@ -0,0 +1,9 @@
+﻿using WebFramework.Infrastructure;
+
+namespace Pwa.Application.Contracts.Account.Role
+{
+    public record EditRoleDto : CreateRoleDto, IDto
+    {
+        public int Id { get; init; }
+    }
+}
