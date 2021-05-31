0cf2482991d1e0378e2b0c8692e9af3db9efe3d2
arminatwork <workwitharmin7@gmail.com>
2021-05-31 22:23:49 +0430

    feat added create address data transfer object

---
+++ Pwa.Application.Contracts/Account/Address/CreateAddressDto.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+using WebFramework.Infrastructure;
+using WebFramework.Utilities;
+
+namespace Pwa.Application.Contracts.Account.Address
+{
+    public record CreateAddressDto : IDto
+    {
+        [Display(Name = "نام")]
+        [Required(ErrorMessage = ValidationMessages.Required)]
+        public string Name { get; init; }
+    }
+}
