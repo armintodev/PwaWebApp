815aac9edc975e3dc00ed1673b6644d524dbc103
arminatwork <workwitharmin7@gmail.com>
2021-05-31 22:32:45 +0430

    feat added comment data transfer object

---
+++ Pwa.Application.Contracts/Product/Comment/CommentDto.cs
@@ -0,0 +1,21 @@
+﻿using WebFramework.Enums;
+using WebFramework.Infrastructure;
+
+namespace Pwa.Application.Contracts.Product.Comment
+{
+    public record CommentDto : IDto
+    {
+        public int Id { get; init; }
+        public string Description { get; init; }
+        public bool IsDeveloper { get; init; }
+        public StatusDto Status { get; init; }
+        public int UserId { get; init; }
+        public int WebApplicationId { get; init; }
+        public string UserFullName { get; init; }
+        public string UserPhoneNumber { get; init; }
+        public string WebAppName { get; init; }
+        public string WebAppAddress { get; init; }
+
+        //public WebApplicationDto WebApplication { get; init; }
+    }
+}
