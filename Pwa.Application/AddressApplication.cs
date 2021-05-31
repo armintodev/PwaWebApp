138117a01f8437a5b0ddd7895065b07e6218e687
arminatwork <workwitharmin7@gmail.com>
2021-05-31 22:15:41 +0430

    feat added address application implementation

---
+++ Pwa.Application/AddressApplication.cs
@@ -0,0 +1,14 @@
+﻿using Pwa.Application.Contracts.Account.Address;
+using Pwa.Domain.Account;
+
+namespace Pwa.Application
+{
+    public class AddressApplication : IAddressApplication
+    {
+        private readonly IAddressRepository _address;
+        public AddressApplication(IAddressRepository address)
+        {
+            _address = address;
+        }
+    }
+}
