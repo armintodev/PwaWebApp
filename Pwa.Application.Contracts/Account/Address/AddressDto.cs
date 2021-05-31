31932ab4dc80c62c23050e05aa100389240b2c26
arminatwork <workwitharmin7@gmail.com>
2021-05-31 22:23:29 +0430

    feat added address data transfer object

---
+++ Pwa.Application.Contracts/Account/Address/AddressDto.cs
@@ -0,0 +1,13 @@
+﻿using WebFramework.Infrastructure;
+
+namespace Pwa.Application.Contracts.Account.Address
+{
+    public record AddressDto : IDto
+    {
+        public int Id { get; init; }
+        public string City { get; init; }
+        public string Province { get; init; }
+        public string Country { get; init; }
+        public string CreationDate { get; init; }
+    }
+}
