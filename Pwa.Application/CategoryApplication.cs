2320cf9cb5f8feb4d8f52f554dbd33a259cee72f
arminatwork <workwitharmin7@gmail.com>
2021-05-31 22:16:00 +0430

    feat added category application implementation

---
+++ Pwa.Application/CategoryApplication.cs
@@ -0,0 +1,14 @@
+﻿using Pwa.Application.Contracts.Product.Category;
+using Pwa.Domain.Product;
+
+namespace Pwa.Application
+{
+    public class CategoryApplication : ICategoryApplication
+    {
+        private readonly ICategoryRepository _category;
+        public CategoryApplication(ICategoryRepository category)
+        {
+            _category = category;
+        }
+    }
+}
