d971df2f89edf32e95a521a7241f72f31c2b776c
arminatwork <workwitharmin7@gmail.com>
2021-05-31 22:21:09 +0430

    feat added role application implementation

---
+++ Pwa.Application/RoleApplication.cs
@@ -0,0 +1,14 @@
+﻿using Pwa.Application.Contracts.Account.Role;
+using Pwa.Domain.Account;
+
+namespace Pwa.Application
+{
+    public class RoleApplication : IRoleApplication
+    {
+        private readonly IRoleRepository _role;
+        public RoleApplication(IRoleRepository role)
+        {
+            _role = role;
+        }
+    }
+}
