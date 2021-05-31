0dc7ba5beb51121afd6b3e9aa0701819d81ddfa6
arminatwork <workwitharmin7@gmail.com>
2021-05-31 22:21:20 +0430

    feat added source site application implementation

---
+++ Pwa.Application/SourceSiteApplication.cs
@@ -0,0 +1,14 @@
+﻿using Pwa.Application.Contracts.Product.SourceSite;
+using Pwa.Domain.Product;
+
+namespace Pwa.Application
+{
+    public class SourceSiteApplication : ISourceSiteApplication
+    {
+        private readonly ISourceSiteRepository _sourceSite;
+        public SourceSiteApplication(ISourceSiteRepository sourceSite)
+        {
+            _sourceSite = sourceSite;
+        }
+    }
+}
