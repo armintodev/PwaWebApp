16d9f05476c53fea1533ebc3ec176cc610f7faaf
arminatwork <workwitharmin7@gmail.com>
2021-05-31 23:19:57 +0430

    feat added address expression

---
+++ Pwa.Infrastructure/Expressions/AddressExpression.cs
@@ -0,0 +1,19 @@
+﻿using Pwa.Application.Contracts.Account.Address;
+using Pwa.Domain.Account;
+using System;
+using System.Linq.Expressions;
+
+namespace Pwa.Infrastructure.EfCore.Expressions
+{
+    public static class AddressExpression
+    {
+        public static Expression<Func<Address, AddressDto>> ToDto => _ => new AddressDto
+        {
+            Id = _.Id,
+            City = _.City,
+            Province = _.Province,
+            Country = _.Country,
+            CreationDate = _.CreationDate.ToString(),
+        };
+    }
+}
