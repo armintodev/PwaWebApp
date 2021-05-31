4ac162f96f92d460e5d22d80aa1e72b39dea1377
arminatwork <workwitharmin7@gmail.com>
2021-05-31 22:29:07 +0430

    feat added statistic search data transfer object

---
+++ Pwa.Application.Contracts/Account/Statistic/StatisticSearchDto.cs
@@ -0,0 +1,11 @@
+﻿using WebFramework.Infrastructure;
+
+namespace Pwa.Application.Contracts.Account.Statistic
+{
+    public record StatisticSearchDto : IDto
+    {
+        public string Browser { get; init; }
+        public string Os { get; init; }
+        public string Device { get; init; }
+    }
+}
