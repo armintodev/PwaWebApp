3193220cd27f493d28c6e2ae64a71a6835c51a5a
arminatwork <workwitharmin7@gmail.com>
2021-05-31 22:21:53 +0430

    feat added user application implementation

---
+++ Pwa.Application/UserApplication.cs
@@ -0,0 +1,14 @@
+﻿using Pwa.Application.Contracts.Account.User;
+using Pwa.Domain.Account;
+
+namespace Pwa.Application
+{
+    public class UserApplication : IUserApplication
+    {
+        private readonly IUserRepository _user;
+        public UserApplication(IUserRepository user)
+        {
+            _user = user;
+        }
+    }
+}
