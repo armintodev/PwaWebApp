2146de148aae3f4edfbae78b7cccd7a89a46d4c7
arminatwork <workwitharmin7@gmail.com>
2021-05-31 22:45:10 +0430

    feat added web application data transfer object

---
+++ Pwa.Application.Contracts/Product/WebApplication/WebApplicationDto.cs
@@ -0,0 +1,24 @@
+﻿using Pwa.Application.Contracts.Account.Developer;
+using Pwa.Application.Contracts.Product.Category;
+using WebFramework.Enums;
+using WebFramework.Infrastructure;
+
+namespace Pwa.Application.Contracts.Product.WebApplication
+{
+    public record WebApplicationDto : IDto
+    {
+        public int Id { get; init; }
+        public string Name { get; init; }
+        public string Description { get; init; }
+        public string WebSiteAddress { get; init; }
+        public byte[] Icons { get; init; }
+        public TypeAddDto TypeAdd { get; init; }
+        public StatusDto Status { get; init; }
+        public int Visit { get; init; }
+        public int Installed { get; init; }
+        public int CategoryId { get; init; }
+        public int DeveloperId { get; init; }
+        //public CategoryDto Category { get; init; }
+        //public DeveloperDto Developer { get; init; }
+    }
+}
