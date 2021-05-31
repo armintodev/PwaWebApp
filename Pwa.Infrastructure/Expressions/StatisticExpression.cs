7ba16ff175522f1198645f325627984d07778318
arminatwork <workwitharmin7@gmail.com>
2021-05-31 23:21:34 +0430

    feat added statistic expression

---
+++ Pwa.Infrastructure/Expressions/StatisticExpression.cs
@@ -0,0 +1,21 @@
+﻿using Pwa.Application.Contracts.Account.Statistic;
+using Pwa.Domain.Account;
+using System;
+using System.Linq.Expressions;
+
+namespace Pwa.Infrastructure.EfCore.Expressions
+{
+    public static class StatisticExpression
+    {
+        public static Expression<Func<Statistic, StatisticDto>> ToDto => _ => new StatisticDto
+        {
+            Id = _.Id,
+            IpAddress = _.IpAddress,
+            Browser = _.Browser,
+            Device = _.Device,
+            Os = _.Os,
+            Version = _.Version,
+            CreationDate = _.CreationDate.ToString()
+        };
+    }
+}
