7d13c3d0ed399ee1c70fada9e06cc956ed4aea45
arminatwork <workwitharmin7@gmail.com>
2021-05-31 22:34:30 +0430

    feat added source site data transfer object

---
+++ Pwa.Application.Contracts/Product/SourceSite/SourceSiteDto.cs
@@ -0,0 +1,17 @@
+﻿using Pwa.Application.Contracts.Account.User;
+using WebFramework.Infrastructure;
+
+namespace Pwa.Application.Contracts.Product.SourceSite
+{
+    public record SourceSiteDto : IDto
+    {
+        public int Id { get; init; }
+        public string Name { get; init; }
+        public string Address { get; init; }
+        public bool IsPwa { get; init; }
+        public int UserId { get; init; }
+        public string UserFullName { get; init; }
+        public string UserPhoneNumber { get; init; }
+        //public UserDto User { get; init; }
+    }
+}
