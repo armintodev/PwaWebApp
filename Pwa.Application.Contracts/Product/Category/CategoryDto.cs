ba4a521bc6572b35823cbe8f9072f531a6c61bab
arminatwork <workwitharmin7@gmail.com>
2021-05-31 22:31:51 +0430

    feat added category data transfer object

---
+++ Pwa.Application.Contracts/Product/Category/CategoryDto.cs
@@ -0,0 +1,10 @@
+﻿using WebFramework.Infrastructure;
+
+namespace Pwa.Application.Contracts.Product.Category
+{
+    public record CategoryDto : IDto
+    {
+        public int Id { get; init; }
+        public string Title { get; init; }
+    }
+}
