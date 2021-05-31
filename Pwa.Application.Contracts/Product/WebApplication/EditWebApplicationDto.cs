58735bec2d12a0aedc8fa69853725d74cde81d0e
arminatwork <workwitharmin7@gmail.com>
2021-05-31 22:45:03 +0430

    feat added edit web application data transfer object

---
+++ Pwa.Application.Contracts/Product/WebApplication/EditWebApplicationDto.cs
@@ -0,0 +1,9 @@
+﻿using WebFramework.Infrastructure;
+
+namespace Pwa.Application.Contracts.Product.WebApplication
+{
+    public record EditWebApplicationDto : IDto
+    {
+        public string Id { get; init; }
+    }
+}
