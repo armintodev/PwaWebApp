dea5c68fe3e21cc334ce75f938db7f64c63cc03d
arminatwork <workwitharmin7@gmail.com>
2021-05-31 22:44:30 +0430

    feat added ticket search data transfer object

---
+++ Pwa.Application.Contracts/Product/Ticket/TicketSearchDto.cs
@@ -0,0 +1,10 @@
+﻿using WebFramework.Infrastructure;
+
+namespace Pwa.Application.Contracts.Product.Ticket
+{
+    public record TicketSearchDto : IDto
+    {
+        public string Title { get; init; }
+        public string Description { get; init; }
+    }
+}
