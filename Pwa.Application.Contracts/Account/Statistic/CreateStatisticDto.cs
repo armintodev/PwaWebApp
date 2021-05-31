cc041a68bda102c9c6f05a88c8da160691b180f3
arminatwork <workwitharmin7@gmail.com>
2021-05-31 22:28:32 +0430

    feat added create statistic data transfer object

---
+++ Pwa.Application.Contracts/Account/Statistic/CreateStatisticDto.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using WebFramework.Infrastructure;
+using WebFramework.Utilities;
+
+namespace Pwa.Application.Contracts.Account.Statistic
+{
+    public record CreateStatisticDto : IDto
+    {
+        [Display(Name = "مرورگر")]
+        [Required(ErrorMessage = ValidationMessages.Required)]
+        public string Browser { get; init; }
+
+        [Display(Name = "دستگاه")]
+        [Required(ErrorMessage = ValidationMessages.Required)]
+        public string Device { get; init; }
+
+        [Display(Name = "سیستم عامل")]
+        [Required(ErrorMessage = ValidationMessages.Required)]
+        public string Os { get; init; }
+
+        [Display(Name = "نسخه")]
+        [Required(ErrorMessage = ValidationMessages.Required)]
+        public string Version { get; init; }
+    }
+}
