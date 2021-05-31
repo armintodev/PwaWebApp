b85b658e204ac2cf756e5613c79a3fe2af87c7f8
arminatwork <workwitharmin7@gmail.com>
2021-05-31 22:21:33 +0430

    feat added statistic application implementation

---
+++ Pwa.Application/StatisticApplication.cs
@@ -0,0 +1,14 @@
+﻿using Pwa.Application.Contracts.Account.Statistic;
+using Pwa.Domain.Account;
+
+namespace Pwa.Application
+{
+    public class StatisticApplication : IStatisticApplication
+    {
+        private readonly IStatisticRepository _statistic;
+        public StatisticApplication(IStatisticRepository statistic)
+        {
+            _statistic = statistic;
+        }
+    }
+}
