5c42cd33ad1ad3939424aba546d425a978748580
arminatwork <workwitharmin7@gmail.com>
2021-05-31 22:21:42 +0430

    feat added ticket application implementation

---
+++ Pwa.Application/TicketApplication.cs
@@ -0,0 +1,14 @@
+﻿using Pwa.Application.Contracts.Product.Ticket;
+using Pwa.Domain.Product;
+
+namespace Pwa.Application
+{
+    public class TicketApplication : ITicketApplication
+    {
+        private readonly ITicketRepository _ticket;
+        public TicketApplication(ITicketRepository ticket)
+        {
+            _ticket = ticket;
+        }
+    }
+}
