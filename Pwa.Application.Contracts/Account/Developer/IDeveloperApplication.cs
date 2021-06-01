a699be360ead6d92f4119bf079e8736a9fe47c1f
arminatwork <workwitharmin7@gmail.com>
2021-06-02 01:10:17 +0430

    refactor added list method signature

--- Pwa.Application.Contracts/Account/Developer/IDeveloperApplication.cs
+++ Pwa.Application.Contracts/Account/Developer/IDeveloperApplication.cs
@@ -1,9 +1,11 @@
-﻿using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace Pwa.Application.Contracts.Account.Developer
 {
     public interface IDeveloperApplication
     {
+        Task<List<DeveloperDto>> ListAsync();
         Task Login(CreateDeveloperDto login);
         Task Register(CreateDeveloperDto register);
         Task Edit(EditDeveloperDto edit);
