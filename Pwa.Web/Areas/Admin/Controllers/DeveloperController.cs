4f77af42eef8d8bce52be20b982247b39834aca8
arminatwork <workwitharmin7@gmail.com>
2021-06-02 01:13:58 +0430

    feat added developer controller, index action

---
+++ Pwa.Web/Areas/Admin/Controllers/DeveloperController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Pwa.Application.Contracts.Account.Developer;
+using System.Threading.Tasks;
+
+namespace Pwa.Web.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class DeveloperController : Controller
+    {
+        private readonly IDeveloperApplication _developer;
+        public DeveloperController(IDeveloperApplication developer)
+        {
+            _developer = developer;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var developers = await _developer.ListAsync();
+            return View(developers);
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        public IActionResult Edit()
+        {
+            return View();
+        }
+    }
+}
