82cffebc6767f255368abd482919fd1b0e21f99d
dariatech <daria@dariatech.ir>
2021-06-02 17:37:24 +0430

    added operation result

---
+++ WebFramework/Utilities/OperationResult.cs
@@ -0,0 +1,14 @@
+﻿namespace WebFramework.Utilities
+{
+    public class OperationResult
+    {
+        public bool Success { get; set; }
+        public string Message { get; set; }
+
+        public OperationResult(bool success, string message)
+        {
+            Success = success;
+            Message = message;
+        }
+    }
+}
