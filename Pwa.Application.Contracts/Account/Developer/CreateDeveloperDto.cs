dd0930b27a2a0f0a38f6b26cdf3b315627322276
dariatech <daria@dariatech.ir>
2021-06-02 17:29:07 +0430

    refactor added address properties,user name property

--- Pwa.Application.Contracts/Account/Developer/CreateDeveloperDto.cs
+++ Pwa.Application.Contracts/Account/Developer/CreateDeveloperDto.cs
@@ -10,6 +10,10 @@ namespace Pwa.Application.Contracts.Account.Developer
         [Required(ErrorMessage = ValidationMessages.Required)]
         public string FullName { get; init; }
 
+        [Display(Name = "نام کاربری")]
+        [Required(ErrorMessage = ValidationMessages.Required)]
+        public string UserName { get; init; }
+
         [Display(Name = "ایمیل")]
         [Required(ErrorMessage = ValidationMessages.Required)]
         public string Email { get; init; }
@@ -20,11 +24,13 @@ namespace Pwa.Application.Contracts.Account.Developer
 
         [Display(Name = "کلمه عبور")]
         [Required(ErrorMessage = ValidationMessages.Required)]
+        [DataType(DataType.Password)]
         public string Password { get; init; }
 
         [Display(Name = "تایید کلمه عبور")]
         [Required(ErrorMessage = ValidationMessages.Required)]
         [Compare(nameof(Password), ErrorMessage = ValidationMessages.CheckPassword)]
+        [DataType(DataType.Password)]
         public string RePassword { get; init; }
 
         [Display(Name = "کدملی")]
@@ -32,7 +38,18 @@ namespace Pwa.Application.Contracts.Account.Developer
         public string NationalCode { get; init; }
         public string Code { get; init; }
 
-        public int AddressId { get; init; }
+        [Display(Name = "شهرستان")]
+        [Required(ErrorMessage = ValidationMessages.Required)]
+        public string City { get; init; }
+
+        [Display(Name = "استان")]
+        [Required(ErrorMessage = ValidationMessages.Required)]
+        public string Province { get; init; }
+
+        [Display(Name = "کشور")]
+        [Required(ErrorMessage = ValidationMessages.Required)]
+        public string Country { get; init; }
+
         public int StatisticId { get; init; }
     }
 }
