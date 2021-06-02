6a6e5189f9d08286f36d71bfdcbf319b6b8b7525
dariatech <daria@dariatech.ir>
2021-06-02 17:29:51 +0430

    refactor added user name property,removed address id property

--- Pwa.Application.Contracts/Account/Developer/DeveloperDto.cs
+++ Pwa.Application.Contracts/Account/Developer/DeveloperDto.cs
@@ -7,13 +7,13 @@ namespace Pwa.Application.Contracts.Account.Developer
     {
         public int Id { get; init; }
         public string FullName { get; init; }
+        public string UserName { get; init; }
         public string NationalCode { get; init; }
         public string Email { get; init; }
         public string PhoneNumber { get; init; }
         public string Code { get; init; }
         public StatusDto Status { get; init; }
         public string CreationDate { get; init; }
-        public int AddressId { get; init; }
         public int StatisticId { get; init; }
     }
 }
