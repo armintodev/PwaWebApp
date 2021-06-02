2cc1764430465f49790ac458a5416e2b0792c299
dariatech <daria@dariatech.ir>
2021-06-02 17:31:02 +0430

    refactor added task operation result to login and register signature

--- Pwa.Application.Contracts/Account/Developer/IDeveloperApplication.cs
+++ Pwa.Application.Contracts/Account/Developer/IDeveloperApplication.cs
@@ -1,13 +1,14 @@
 ﻿using System.Collections.Generic;
 using System.Threading.Tasks;
+using WebFramework.Utilities;
 
 namespace Pwa.Application.Contracts.Account.Developer
 {
     public interface IDeveloperApplication
     {
         Task<List<DeveloperDto>> ListAsync();
-        Task Login(CreateDeveloperDto login);
-        Task Register(CreateDeveloperDto register);
+        Task<OperationResult> Login(CreateDeveloperDto login);
+        Task<OperationResult> Register(CreateDeveloperDto register);
         Task Edit(EditDeveloperDto edit);
         Task Delete(int id);
         Task Activate(int id);
