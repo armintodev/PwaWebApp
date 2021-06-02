da8b543f6cec7c9213e2475cd8d3bfcb3b183acf
arminatwork <workwitharmin7@gmail.com>
2021-06-03 01:32:41 +0430

    feat added login dto

---
+++ Pwa.Application.Contracts/Account/User/LoginDto.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+using WebFramework.Infrastructure;
+using WebFramework.Utilities;
+
+namespace Pwa.Application.Contracts.Account.User
+{
+    public record LoginDto : IDto
+    {
+        [Display(Name = "شماره همراه")]
+        [Required(ErrorMessage = ValidationMessages.Required)]
+        public string PhoneNumber { get; init; }
+    }
+}
