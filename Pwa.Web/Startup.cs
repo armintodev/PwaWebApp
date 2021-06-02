a0a9f73b82fd8708759f1b73f3626bb117410cb3
arminatwork <workwitharmin7@gmail.com>
2021-06-03 01:36:40 +0430

    refactor added authentication,authorization middleware to software

--- Pwa.Web/Startup.cs
+++ Pwa.Web/Startup.cs
@@ -34,6 +34,9 @@ namespace Pwa.Web
 
             app.UseStaticFiles();
 
+            app.UseAuthentication();
+            app.UseAuthorization();
+
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
