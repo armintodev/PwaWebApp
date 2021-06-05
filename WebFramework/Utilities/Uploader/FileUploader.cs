82f12684b5f4b32874276e6deaad81fd08a2b614
arminatwork <workwitharmin7@gmail.com>
2021-06-06 00:35:24 +0430

    feat added file uploader implementation

---
+++ WebFramework/Utilities/Uploader/FileUploader.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using WebFramework.Infrastructure;
+
+namespace WebFramework.Utilities.Uploader
+{
+    public class FileUploader : IFileUploader
+    {
+        private readonly IWebHostEnvironment _webHost;
+        public FileUploader(IWebHostEnvironment webHost)
+        {
+            _webHost = webHost;
+        }
+
+        public async Task<string> Upload(IFormFile file, string path)
+        {
+            if (file is null) return "";
+
+            //for example: Photos//Developer//fileName
+            var directoryPath = $"{_webHost.WebRootPath}//lib//Photos//{path}";
+
+            if (!Directory.Exists(directoryPath))
+                Directory.CreateDirectory(directoryPath);
+
+            var fileName = $"{DateTime.Now.ToFileName()}-{file.FileName}";
+            var filePath = $"{directoryPath}//{fileName}";
+            await using var output = File.Create(filePath);
+            await file.CopyToAsync(output);
+            return $"{path}/{fileName}";
+        }
+    }
+}
