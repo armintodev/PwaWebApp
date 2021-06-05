3be51dd58125acf1e8c8cee8886177bb177cfb23
arminatwork <workwitharmin7@gmail.com>
2021-06-05 17:10:49 +0430

    refactor added add implementation method

--- Pwa.Application/StatisticApplication.cs
+++ Pwa.Application/StatisticApplication.cs
@@ -1,14 +1,40 @@
-﻿using Pwa.Application.Contracts.Account.Statistic;
+﻿using System;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Pwa.Application.Contracts.Account.Statistic;
 using Pwa.Domain.Account;
+using WebFramework.Utilities;
 
 namespace Pwa.Application
 {
     public class StatisticApplication : IStatisticApplication
     {
         private readonly IStatisticRepository _statistic;
-        public StatisticApplication(IStatisticRepository statistic)
+        private readonly IHttpContextAccessor _accessor;
+        public StatisticApplication(IStatisticRepository statistic, IHttpContextAccessor accessor)
         {
             _statistic = statistic;
+            _accessor = accessor;
+        }
+
+        public async Task<OperationResult> Add(CreateStatisticDto createStatistic)
+        {
+            if (createStatistic is null) return new OperationResult(false, "اطلاعات نمی تواند خالی باشد");
+
+            // I use this when authentication is right
+            var userId = _accessor.HttpContext?.User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+            Statistic statistic =
+                new(createStatistic.IpAddress, createStatistic.Path, createStatistic.Browser, createStatistic.Device,
+                    createStatistic.Os, createStatistic.Version, Convert.ToInt32(userId));
+
+            await _statistic.AddAsync(statistic, CancellationToken.None);
+            await _statistic.SaveChangesAsync();
+
+            return new OperationResult(true, "اطلاعات با موفقیت اضافه شد.");
         }
     }
 }
