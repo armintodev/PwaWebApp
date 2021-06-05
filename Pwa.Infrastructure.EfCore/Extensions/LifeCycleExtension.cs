d37b472daec671cbd1142acddf4bc31a8cf833ea
arminatwork <workwitharmin7@gmail.com>
2021-06-05 17:13:13 +0430

    refactor added statistic application life cycle

--- Pwa.Infrastructure.EfCore/Extensions/LifeCycleExtension.cs
+++ Pwa.Infrastructure.EfCore/Extensions/LifeCycleExtension.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using Pwa.Application;
 using Pwa.Application.Contracts.Account.Developer;
+using Pwa.Application.Contracts.Account.Statistic;
 using Pwa.Domain.Account;
 using Pwa.Domain.Product;
 using Pwa.Infrastructure.EfCore.Repositories;
@@ -15,7 +16,10 @@ namespace Pwa.Infrastructure.EfCore.Extensions
             service.AddScoped<IDeveloperApplication, DeveloperApplication>();
 
             service.AddScoped<IRoleRepository, RoleRepository>();
+
             service.AddScoped<IStatisticRepository, StatisticRepository>();
+            service.AddScoped<IStatisticApplication, StatisticApplication>();
+
             service.AddScoped<IUserRepository, UserRepository>();
             service.AddScoped<ICategoryRepository, CategoryRepository>();
             service.AddScoped<ICommentRepository, CommentRepository>();
