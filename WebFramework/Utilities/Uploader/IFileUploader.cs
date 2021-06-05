b8f038f21291590cb85acab4e265190144602fc9
arminatwork <workwitharmin7@gmail.com>
2021-06-06 00:35:04 +0430

    feat added file uploader signature

---
+++ WebFramework/Utilities/Uploader/IFileUploader.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace WebFramework.Utilities.Uploader
+{
+    public interface IFileUploader
+    {
+        public Task<string> Upload(IFormFile file, string path);
+    }
+}
