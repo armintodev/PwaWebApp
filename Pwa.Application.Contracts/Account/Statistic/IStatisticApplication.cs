93eb12fa8c8b37e5f1f4580a03261a472bdf0c60
arminatwork <workwitharmin7@gmail.com>
2021-06-05 17:11:17 +0430

    refactor added add signature method

--- Pwa.Application.Contracts/Account/Statistic/IStatisticApplication.cs
+++ Pwa.Application.Contracts/Account/Statistic/IStatisticApplication.cs
@@ -1,8 +1,10 @@
-﻿using WebFramework.Infrastructure;
+﻿using System.Threading.Tasks;
+using WebFramework.Utilities;
 
 namespace Pwa.Application.Contracts.Account.Statistic
 {
     public interface IStatisticApplication
     {
+        Task<OperationResult> Add(CreateStatisticDto createStatistic);
     }
 }
