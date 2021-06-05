074fad3918a6184a8a8200b1ec59fab7485362fc
arminatwork <workwitharmin7@gmail.com>
2021-06-05 17:15:09 +0430

    feat added need information attribute to got it when put on a method

---
+++ Pwa.Web/Filters/NeedInformationAttribute.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.DependencyInjection;
+using Pwa.Application.Contracts.Account.Statistic;
+using Wangkanai.Detection.Services;
+
+namespace Pwa.Web.Filters
+{
+    [AttributeUsage(AttributeTargets.Method)]
+    public class NeedInformationAttribute : Attribute, IAsyncActionFilter
+    {
+        public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+            var service = context.HttpContext.RequestServices.GetRequiredService<IDetectionService>();
+            var statisticApplication = context.HttpContext.RequestServices.GetRequiredService<IStatisticApplication>();
+
+            var ipAddress = context.HttpContext.Connection.RemoteIpAddress?.ToString();
+            var requestPath = context.HttpContext.Request.Path;
+
+            CreateStatisticDto statistic = new()
+            {
+                Browser = service.Browser.Name.ToString(),
+                Device = service.Device.Type.ToString(),
+                Os = service.Platform.Name.ToString(),
+                Version = service.Browser.Version.ToString(),
+                IpAddress = ipAddress,
+                Path = requestPath
+            };
+
+            var result = await statisticApplication.Add(statistic);
+            if (result.Success) await next.Invoke();
+        }
+    }
+}
