99a88d16da3b5f163c2e9d47e2eed552ddf5d689
arminatwork <workwitharmin7@gmail.com>
2021-06-06 19:58:46 +0430

    refactor added missing properties,removed inherited of create developer dto

--- Pwa.Application.Contracts/Account/Developer/EditDeveloperDto.cs
+++ Pwa.Application.Contracts/Account/Developer/EditDeveloperDto.cs
@@ -1,9 +1,35 @@
-﻿using WebFramework.Infrastructure;
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Http;
+using WebFramework.Infrastructure;
+using WebFramework.Utilities;
 
 namespace Pwa.Application.Contracts.Account.Developer
 {
-    public record EditDeveloperDto : CreateDeveloperDto, IDto
+    public record EditDeveloperDto : IDto
     {
         public int Id { get; init; }
+
+        [Display(Name = "نام کامل")]
+        [Required(ErrorMessage = ValidationMessages.Required)]
+        public string FullName { get; init; }
+
+        [Display(Name = "کدملی")]
+        [Required(ErrorMessage = ValidationMessages.Required)]
+        public string NationalCode { get; init; }
+
+        [Display(Name = "شهرستان")]
+        [Required(ErrorMessage = ValidationMessages.Required)]
+        public string City { get; init; }
+
+        [Display(Name = "استان")]
+        [Required(ErrorMessage = ValidationMessages.Required)]
+        public string Province { get; init; }
+
+        [Display(Name = "کشور")]
+        [Required(ErrorMessage = ValidationMessages.Required)]
+        public string Country { get; init; }
+
+        [Display(Name = "تصویر پروفایل")]
+        public IFormFile ProfileUrl { get; init; }
     }
 }
