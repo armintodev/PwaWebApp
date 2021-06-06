1c6df558c68089cdbf4731fc15b3bb77c0b203d1
arminatwork <workwitharmin7@gmail.com>
2021-06-06 20:05:23 +0430

    refactor added operation result generic to get data,clean coding

--- WebFramework/Utilities/OperationResult.cs
+++ WebFramework/Utilities/OperationResult.cs
@@ -5,10 +5,19 @@
         public bool Success { get; set; }
         public string Message { get; set; }
 
-        public OperationResult(bool success, string message)
+        public OperationResult(bool success = true, string message = "")
         {
             Success = success;
             Message = message;
+        }
+    }
+
+    public class OperationResult<TData> : OperationResult where TData : class
+    {
+        public TData Data { get; set; }
+        public OperationResult(TData data, bool success = true, string message = "") : base(success, message)
+        {
+            Data = data;
         }
     }
 }
