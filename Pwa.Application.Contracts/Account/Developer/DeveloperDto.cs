8b55a17adf898485e4d20084743398fd050d2e09
arminatwork <workwitharmin7@gmail.com>
2021-06-06 19:58:07 +0430

    refactor added display attribute on properties dto,added missing properties

--- Pwa.Application.Contracts/Account/Developer/DeveloperDto.cs
+++ Pwa.Application.Contracts/Account/Developer/DeveloperDto.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using WebFramework.Enums;
 using WebFramework.Infrastructure;
 
@@ -7,15 +8,46 @@ namespace Pwa.Application.Contracts.Account.Developer
     public record DeveloperDto : IDto
     {
         public int Id { get; init; }
+
+        [Display(Name = "نام کامل")]
         public string FullName { get; init; }
+
+        [Display(Name = "نام کاربری")]
         public string UserName { get; init; }
+
+        [Display(Name = "کدملی")]
         public string NationalCode { get; init; }
+
+        [Display(Name = "ایمیل")]
         public string Email { get; init; }
+
+        [Display(Name = "شماره همراه")]
         public string PhoneNumber { get; init; }
-        public IFormFile ProfileUrl { get; init; }
+
+        [Display(Name = "شهر")]
+        public string City { get; init; }
+
+        [Display(Name = "استان")]
+        public string Province { get; init; }
+
+        [Display(Name = "کشور")]
+        public string Country { get; init; }
+
+        [Display(Name = "تصویر پروفایل")]
+        public string ProfileUrl { get; init; }
+
+        [Display(Name = "کد فعال سازی")]
         public string Code { get; init; }
+
+        [Display(Name = "وضعیت")]
         public StatusDto Status { get; init; }
+
+        [Display(Name = "تاریخ ثبت نام")]
         public string CreationDate { get; init; }
-        public int? StatisticId { get; init; }
+
+        [Display(Name = "تاریخ آخرین ویرایش")]
+        public string LastEditDate { get; init; }
+
+        public List<int>? StatisticId { get; init; }
     }
 }
