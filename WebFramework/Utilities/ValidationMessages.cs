413dbbb873f0f397fec6b5d6e6930f75fc59b38a
arminatwork <workwitharmin7@gmail.com>
2021-06-06 20:05:41 +0430

    refactor added wrong email constants

--- WebFramework/Utilities/ValidationMessages.cs
+++ WebFramework/Utilities/ValidationMessages.cs
@@ -5,6 +5,7 @@
         public const string Required = "مقدار {0} نباید خالی باشد.";
         public const string CheckPassword = "پسورد ها مطابقت ندارند";
         public const string WrongPhoneNumber = "شماره همراه درست وارد کنید.";
+        public const string WrongEmail = "ایمیل را درست وارد کنید.";
         public const string WrongMaxLength = "مقدار {0} نمیتواند از {1} بیشتر باشد.";
     }
 }
