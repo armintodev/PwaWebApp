2f0a3ee074d79c67a8cd3ed2c07ecae9f8dfd49f
arminatwork <workwitharmin7@gmail.com>
2021-06-08 00:57:01 +0430

    feat added random generator code

---
+++ WebFramework/Utilities/RandomGenerator.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Text;
+
+namespace WebFramework.Utilities
+{
+    public static class RandomGenerator
+    {
+        public static string Generate(string symbol)
+        {
+            return $"{symbol}{RandomString()}{RandomNumber()}";
+        }
+
+        public static string Generate()
+        {
+            return RandomNumber();
+        }
+
+        private static string RandomString()
+        {
+            const int length = 2;
+
+            var strBuild = new StringBuilder();
+            var random = new Random();
+
+            for (var i = 0; i <= length; i++)
+            {
+                var flt = random.NextDouble();
+                var shift = Convert.ToInt32(Math.Floor(25 * flt));
+                var letter = Convert.ToChar(shift + 65);
+                strBuild.Append(letter);
+            }
+
+            return strBuild.ToString();
+        }
+
+        private static string RandomNumber()
+        {
+            var random = new Random();
+            return random.Next(10000, 99999).ToString();
+        }
+    }
+}
