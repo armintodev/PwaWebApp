cdf0e4524f4f252264af239b4deb1feaeed0dee4
dariatech <daria@dariatech.ir>
2021-06-08 16:34:55 +0430

    feat added picture domain model

---
+++ Pwa.Domain/Product/Picture.cs
@@ -0,0 +1,26 @@
+﻿using Pwa.Domain.Aggregates;
+using Pwa.Domain.Base;
+using System;
+
+namespace Pwa.Domain.Product
+{
+    public class Picture : BaseDetail, IPictureAggregate
+    {
+        public string FileName { get; private set; }
+
+        public int WebApplicationId { get; private set; }
+        public WebApplication WebApplication { get; private set; }
+
+        protected Picture()
+        {
+
+        }
+
+        public Picture(string fileName, int webApplicationId)
+        {
+            FileName = fileName;
+            WebApplicationId = webApplicationId;
+            CreationDate = DateTime.Now;
+        }
+    }
+}
