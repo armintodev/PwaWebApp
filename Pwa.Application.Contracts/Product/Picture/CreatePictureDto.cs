c83f629a976b94f27e079038fe8138d192179dab
dariatech <daria@dariatech.ir>
2021-06-08 16:32:43 +0430

    feat added create picture dto

---
+++ Pwa.Application.Contracts/Product/Picture/CreatePictureDto.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.ComponentModel.DataAnnotations;
+using WebFramework.Utilities;
+
+namespace Pwa.Application.Contracts.Product.Picture
+{
+    public class CreatePictureDto
+    {
+        [Display(Name = "تصویر")]
+        [DataType(DataType.ImageUrl)]
+        [Required(ErrorMessage = ValidationMessages.Required)]
+        public IFormFile File { get; set; }
+    }
+}
