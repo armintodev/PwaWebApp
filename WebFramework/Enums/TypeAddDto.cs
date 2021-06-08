c99888dd68528aea27718bfa9c9d51612fbad334
dariatech <daria@dariatech.ir>
2021-06-08 16:39:44 +0430

    refactor added display data annotation

--- WebFramework/Enums/TypeAddDto.cs
+++ WebFramework/Enums/TypeAddDto.cs
@@ -1,8 +1,13 @@
-﻿namespace WebFramework.Enums
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebFramework.Enums
 {
     public enum TypeAddDto
     {
+        [Display(Name = "دستی")]
         Custom = 0,
+
+        [Display(Name = "خودکار")]
         Auto = 1
     }
 }
