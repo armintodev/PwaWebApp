d9285739b5d892d6869b863404cd6198ea53b2d0
arminatwork <workwitharmin7@gmail.com>
2021-06-08 23:14:01 +0430

    feat added url path request constants

---
+++ WebFramework/Utilities/UrlPathRequest.cs
@@ -0,0 +1,11 @@
+﻿namespace WebFramework.Utilities
+{
+    public static class UrlPathRequest
+    {
+        public const string AdminUser = "/Admin/User";
+        public const string AdminDeveloper = "/Admin/Developer";
+        public const string AdminCategory = "/Admin/Category";
+        public const string AdminWebApplication = "/Admin/WebApplication";
+        public const string AdminTicket = "/Admin/Ticket";
+    }
+}
