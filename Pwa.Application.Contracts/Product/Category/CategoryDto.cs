0c40292a40613afcf2b4cb566621ae14ae603a1e
dariatech <daria@dariatech.ir>
2021-06-08 18:13:39 +0430

    refactor added date models properties,added display data annotations

--- Pwa.Application.Contracts/Product/Category/CategoryDto.cs
+++ Pwa.Application.Contracts/Product/Category/CategoryDto.cs
@@ -1,10 +1,20 @@
-﻿using WebFramework.Infrastructure;
+﻿using System.ComponentModel.DataAnnotations;
+using WebFramework.Infrastructure;
 
 namespace Pwa.Application.Contracts.Product.Category
 {
     public record CategoryDto : IDto
     {
+        [Display(Name = "آی دی")]
         public int Id { get; init; }
+
+        [Display(Name = "عنوان")]
         public string Title { get; init; }
+
+        [Display(Name = "تاریخ ایجاد")]
+        public string CreationDate { get; init; }
+
+        [Display(Name = "تاریخ ویرایش")]
+        public string LastEditDate { get; init; }
     }
 }
