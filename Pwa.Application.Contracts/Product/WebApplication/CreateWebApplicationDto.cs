91d17ee74c6b7ceb526728c931c55057a6586fe9
dariatech <daria@dariatech.ir>
2021-06-08 16:33:25 +0430

    refactor added constructor,implement Pictures

--- Pwa.Application.Contracts/Product/WebApplication/CreateWebApplicationDto.cs
+++ Pwa.Application.Contracts/Product/WebApplication/CreateWebApplicationDto.cs
@@ -1,4 +1,9 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Pwa.Application.Contracts.Product.Category;
+using Pwa.Application.Contracts.Product.Picture;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using WebFramework.Enums;
 using WebFramework.Infrastructure;
 using WebFramework.Utilities;
@@ -7,6 +12,11 @@ namespace Pwa.Application.Contracts.Product.WebApplication
 {
     public record CreateWebApplicationDto : IDto
     {
+        public CreateWebApplicationDto()
+        {
+            Pictures = new List<CreatePictureDto>();
+        }
+
         [Display(Name = "نام")]
         [Required(ErrorMessage = ValidationMessages.Required)]
         [MaxLength(500, ErrorMessage = ValidationMessages.WrongMaxLength)]
@@ -19,10 +29,6 @@ namespace Pwa.Application.Contracts.Product.WebApplication
         [Display(Name = "آدرس")]
         [Required(ErrorMessage = ValidationMessages.Required)]
         public string WebSiteAddress { get; init; }
-
-        [Display(Name = "آیکون")]
-        [Required(ErrorMessage = ValidationMessages.Required)]
-        public byte[] Icons { get; init; }
 
         [Display(Name = "نوع اضافه کردن")]
         [Required(ErrorMessage = ValidationMessages.Required)]
@@ -40,7 +46,14 @@ namespace Pwa.Application.Contracts.Product.WebApplication
         [Required(ErrorMessage = ValidationMessages.Required)]
         public int Installed { get; init; }
 
+        [Range(1, int.MaxValue, ErrorMessage = ValidationMessages.WrongCategory)]
         public int CategoryId { get; init; }
+        public SelectList Categories { get; init; }
+        public CategoryDto Category { get; init; }
+
         public int DeveloperId { get; init; }
+
+        public List<CreatePictureDto> Pictures { get; init; }
+        public IFormFileCollection Files { get; set; }
     }
 }
