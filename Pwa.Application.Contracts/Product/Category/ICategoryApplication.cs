49b5035248ef44435a5df6192180e6db07690103
dariatech <daria@dariatech.ir>
2021-06-08 16:32:26 +0430

    refactor added list method signature

--- Pwa.Application.Contracts/Product/Category/ICategoryApplication.cs
+++ Pwa.Application.Contracts/Product/Category/ICategoryApplication.cs
@@ -1,6 +1,10 @@
-﻿namespace Pwa.Application.Contracts.Product.Category
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Pwa.Application.Contracts.Product.Category
 {
     public interface ICategoryApplication
     {
+        Task<List<CategoryDto>> List();
     }
 }
