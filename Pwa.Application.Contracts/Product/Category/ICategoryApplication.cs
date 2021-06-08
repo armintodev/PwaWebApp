2b01604ecfa6f5dd614e2825c3a462cd7008f08a
dariatech <daria@dariatech.ir>
2021-06-08 18:14:14 +0430

    refactor added create, get, detail, delete, edit operations signature

--- Pwa.Application.Contracts/Product/Category/ICategoryApplication.cs
+++ Pwa.Application.Contracts/Product/Category/ICategoryApplication.cs
@@ -1,10 +1,16 @@
 ﻿using System.Collections.Generic;
 using System.Threading.Tasks;
+using WebFramework.Utilities;
 
 namespace Pwa.Application.Contracts.Product.Category
 {
     public interface ICategoryApplication
     {
         Task<List<CategoryDto>> List();
+        Task<OperationResult> Create(CreateCategoryDto dto);
+        Task<OperationResult<EditCategoryDto>> Get(int id);
+        Task<OperationResult<CategoryDto>> Detail(int id);
+        Task<OperationResult> Delete(int id);
+        Task<OperationResult> Edit(EditCategoryDto dto);
     }
 }
