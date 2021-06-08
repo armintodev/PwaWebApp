3643356d7ae895d85fd44a6a5ef9cacf5091015e
dariatech <daria@dariatech.ir>
2021-06-08 16:33:52 +0430

    refactor added display data annotation on properties

--- Pwa.Application.Contracts/Product/WebApplication/WebApplicationDto.cs
+++ Pwa.Application.Contracts/Product/WebApplication/WebApplicationDto.cs
@@ -1,5 +1,6 @@
 ﻿using Pwa.Application.Contracts.Account.Developer;
 using Pwa.Application.Contracts.Product.Category;
+using System.ComponentModel.DataAnnotations;
 using WebFramework.Enums;
 using WebFramework.Infrastructure;
 
@@ -7,15 +8,33 @@ namespace Pwa.Application.Contracts.Product.WebApplication
 {
     public record WebApplicationDto : IDto
     {
+        [Display(Name = "آی دی")]
         public int Id { get; init; }
+
+        [Display(Name = "نام")]
         public string Name { get; init; }
+
+        [Display(Name = "توضیحات")]
         public string Description { get; init; }
+
+        [Display(Name = "آدرس وب سایت")]
         public string WebSiteAddress { get; init; }
-        public byte[] Icons { get; init; }
+
+        [Display(Name = "نوع اضافه شده")]
         public TypeAddDto TypeAdd { get; init; }
+
+        [Display(Name = "وضعیت")]
         public StatusDto Status { get; init; }
+
+        [Display(Name = "بازدید")]
         public int Visit { get; init; }
+
+        [Display(Name = "دریافت شده")]
         public int Installed { get; init; }
+
+        [Display(Name = "تاریخ ایجاد محصول")]
+        public string CreationDate { get; init; }
+
         public int CategoryId { get; init; }
         public int DeveloperId { get; init; }
         //public CategoryDto Category { get; init; }
