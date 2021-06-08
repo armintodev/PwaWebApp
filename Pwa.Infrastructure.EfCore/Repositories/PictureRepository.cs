991b0a70b400b58f6b8cc3688c92e8ed6d7ee235
dariatech <daria@dariatech.ir>
2021-06-08 16:37:20 +0430

    feat added picture repository implementation

---
+++ Pwa.Infrastructure.EfCore/Repositories/PictureRepository.cs
@@ -0,0 +1,14 @@
+﻿using Pwa.Domain.Product;
+using WebFramework.Infrastructure;
+
+namespace Pwa.Infrastructure.EfCore.Repositories
+{
+    public class PictureRepository : BaseRepository<Picture>, IPictureRepository
+    {
+        private readonly ApplicationDbContext _context;
+        public PictureRepository(ApplicationDbContext context) : base(context)
+        {
+            _context = context;
+        }
+    }
+}
