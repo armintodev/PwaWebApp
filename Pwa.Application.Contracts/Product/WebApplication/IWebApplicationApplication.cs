f3eb1133bd127c4a60d3ff683d6562e83eca0734
dariatech <daria@dariatech.ir>
2021-06-08 16:32:00 +0430

    refactor added operations list,create method signature

--- Pwa.Application.Contracts/Product/WebApplication/IWebApplicationApplication.cs
+++ Pwa.Application.Contracts/Product/WebApplication/IWebApplicationApplication.cs
@@ -1,6 +1,12 @@
-﻿namespace Pwa.Application.Contracts.Product.WebApplication
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using WebFramework.Utilities;
+
+namespace Pwa.Application.Contracts.Product.WebApplication
 {
     public interface IWebApplicationApplication
     {
+        Task<OperationResult> Create(CreateWebApplicationDto dto);
+        Task<List<WebApplicationDto>> List();
     }
 }
