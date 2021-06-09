5eb1e737ab1870db1f92e559d8dd76fa20887687
dariatech <daria@dariatech.ir>
2021-06-09 16:50:53 +0430

    refactor inherited from create ticket dto ,added missing properties

--- Pwa.Application.Contracts/Product/Ticket/EditTicketDto.cs
+++ Pwa.Application.Contracts/Product/Ticket/EditTicketDto.cs
@@ -1,9 +1,16 @@
-﻿using WebFramework.Infrastructure;
+﻿using System.ComponentModel.DataAnnotations;
+using WebFramework.Infrastructure;
 
 namespace Pwa.Application.Contracts.Product.Ticket
 {
-    public record EditTicketDto : IDto
+    public record EditTicketDto : CreateTicketDto, IDto
     {
         public int Id { get; init; }
+
+        [Display(Name = "نام توسعه دهنده")]
+        public string DeveloperFullName { get; init; }
+
+        [Display(Name = "ایمیل توسعه دهنده")]
+        public string DeveloperEmail { get; init; }
     }
 }
