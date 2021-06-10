90c9e58d9cf0829b19c5535ba9f1d934dca67618
arminatwork <workwitharmin7@gmail.com>
2021-06-11 04:08:48 +0430

    feat added role status dto

---
+++ WebFramework/Enums/RoleStatusDto.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebFramework.Enums
+{
+    public enum RoleStatusDto
+    {
+        [Display(Name = "مدیر")]
+        Admin = 0,
+
+        [Display(Name = "کاربر عادی")]
+        Basic = 1,
+
+        [Display(Name = "توسعه دهنده")]
+        Developer = 2
+    }
+}
