8347251638284264d2261951bf13fc2a95fb7d51
arminatwork <workwitharmin7@gmail.com>
2021-06-11 03:57:56 +0430

    refactor added display name,creation and last edit date properties

--- Pwa.Domain/Account/Role.cs
+++ Pwa.Domain/Account/Role.cs
@@ -1,13 +1,27 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using System;
+using Microsoft.AspNetCore.Identity;
 using Pwa.Domain.Aggregates;
 
 namespace Pwa.Domain.Account
 {
     public class Role : IdentityRole<int>, IRoleAggregate
     {
-        public Role(string name)
+        public string DisplayName { get; private set; }
+        public DateTime CreationDate { get; private set; }
+        public DateTime LastEditDate { get; private set; }
+        public Role(string name, string displayName)
         {
             Name = name;
+            DisplayName = displayName;
+            CreationDate = DateTime.Now;
+            LastEditDate = DateTime.Now;
+        }
+
+        public void Edit(string name, string displayName)
+        {
+            Name = name;
+            DisplayName = displayName;
+            LastEditDate = DateTime.Now;
         }
     }
 }
