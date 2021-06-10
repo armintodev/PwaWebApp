37da110975a93737a678ef67c5328578d000e0e8
arminatwork <workwitharmin7@gmail.com>
2021-06-11 03:56:26 +0430

    refactor removed unused properties,added full name

--- Pwa.Application.Contracts/Account/User/CreateUserDto.cs
+++ Pwa.Application.Contracts/Account/User/CreateUserDto.cs
@@ -8,17 +8,12 @@ namespace Pwa.Application.Contracts.Account.User
     {
         [Display(Name = "شماره همراه")]
         [Required(ErrorMessage = ValidationMessages.Required)]
-        [RegularExpression("", ErrorMessage = ValidationMessages.WrongPhoneNumber)]
+        // [RegularExpression("", ErrorMessage = ValidationMessages.WrongPhoneNumber)]
         public string PhoneNumber { get; init; }
 
-        [Display(Name = "کلمه عبور")]
-        [Required(ErrorMessage = ValidationMessages.Required)]
-        public string Password { get; init; }
+        [Display(Name = "نام کامل")]
+        public string FullName { get; init; }
 
-        [Display(Name = "تایید کلمه عبور")]
-        [Required(ErrorMessage = ValidationMessages.Required)]
-        [Compare(nameof(Password), ErrorMessage = ValidationMessages.CheckPassword)]
-        public string RePassword { get; init; }
         public string Code { get; init; }
 
         public string Role { get; set; }
