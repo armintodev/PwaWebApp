90bc77458973be036aae80513483ac1bbae5922a
arminatwork <workwitharmin7@gmail.com>
2021-06-11 03:52:51 +0430

    refactor added missing properties

--- Pwa.Application.Contracts/Account/Role/RoleDto.cs
+++ Pwa.Application.Contracts/Account/Role/RoleDto.cs
@@ -1,10 +1,23 @@
-﻿using WebFramework.Infrastructure;
+﻿using System.ComponentModel.DataAnnotations;
+using WebFramework.Infrastructure;
 
 namespace Pwa.Application.Contracts.Account.Role
 {
     public record RoleDto : IDto
     {
+        [Display(Name = "آی دی")]
         public int Id { get; init; }
+
+        [Display(Name = "نام")]
         public string Name { get; init; }
+
+        [Display(Name = "نام نمایشی")]
+        public string DisplayName { get; init; }
+
+        [Display(Name = "تاریخ ایجاد")]
+        public string CreationDate { get; init; }
+
+        [Display(Name = "تاریخ آخرین ویرایش")]
+        public string LastEditDate { get; init; }
     }
 }
