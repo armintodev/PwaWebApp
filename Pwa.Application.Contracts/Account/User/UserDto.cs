682b941596b5437537364c5d0b8c0b2eb2253932
arminatwork <workwitharmin7@gmail.com>
2021-06-11 03:57:16 +0430

    refactor added missing properties

--- Pwa.Application.Contracts/Account/User/UserDto.cs
+++ Pwa.Application.Contracts/Account/User/UserDto.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using System.ComponentModel.DataAnnotations;
+using Pwa.Application.Contracts.Account.Developer;
 using WebFramework.Enums;
 using WebFramework.Infrastructure;
 
@@ -6,12 +7,36 @@ namespace Pwa.Application.Contracts.Account.User
 {
     public record UserDto : IDto
     {
+        [Display(Name = "آی دی")]
         public int Id { get; init; }
+
+        [Display(Name = "نام کامل")]
         public string FullName { get; init; }
+
+        [Display(Name = "نام کاربری")]
+        public string UserName { get; init; }
+
+        [Display(Name = "ایمیل")]
         public string Email { get; init; }
+
+        [Display(Name = "شماره همراه")]
         public string PhoneNumber { get; init; }
+
+        [Display(Name = "کد فعال سازی")]
         public string Code { get; init; }
+
+        [Display(Name = "وضعیت")]
         public StatusDto Status { get; init; }
+
+        [Display(Name = "نقش")]
+        public RoleStatusDto Role { get; init; }
+
+        [Display(Name = "تاریخ ایجاد")]
         public string CreationDate { get; init; }
+
+        [Display(Name = "تاریخ آخرین ویرایش")]
+        public string LastEditDate { get; init; }
+
+        public DeveloperDto Developer { get; init; }
     }
 }
