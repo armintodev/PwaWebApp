dcef4c6c9b2aa1835639381ae25ac24d6f326e5e
arminatwork <workwitharmin7@gmail.com>
2021-06-11 03:54:54 +0430

    refactor added crud operation + active-deactive user operations signature

--- Pwa.Application.Contracts/Account/User/IUserApplication.cs
+++ Pwa.Application.Contracts/Account/User/IUserApplication.cs
@@ -1,8 +1,19 @@
-﻿using WebFramework.Infrastructure;
+﻿using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using WebFramework.Utilities;
 
 namespace Pwa.Application.Contracts.Account.User
 {
     public interface IUserApplication
     {
+        Task<OperationResult> Register(AuthDto dto, CancellationToken cancellationToken);
+        Task<List<UserDto>> List();
+        Task<OperationResult<EditUserDto>> Get(int id, CancellationToken cancellationToken);
+        Task<OperationResult> Edit(EditUserDto dto, CancellationToken cancellationToken);
+        Task<OperationResult<UserDto>> Detail(int id, CancellationToken cancellationToken);
+        Task<OperationResult> Delete(int id, CancellationToken cancellationToken);
+        Task Activate(int id, CancellationToken cancellationToken);
+        Task DeActivate(int id, CancellationToken cancellationToken);
     }
 }
