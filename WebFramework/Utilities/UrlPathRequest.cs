d2069a6350fab04d05fd82031a1fc6ca693a7516
arminatwork <workwitharmin7@gmail.com>
2021-06-11 04:09:06 +0430

    refactor added admin role url path request

--- WebFramework/Utilities/UrlPathRequest.cs
+++ WebFramework/Utilities/UrlPathRequest.cs
@@ -3,6 +3,7 @@
     public static class UrlPathRequest
     {
         public const string AdminUser = "/Admin/User";
+        public const string AdminRole = "/Admin/Role";
         public const string AdminDeveloper = "/Admin/Developer";
         public const string AdminCategory = "/Admin/Category";
         public const string AdminWebApp = "/Admin/WebApp";
