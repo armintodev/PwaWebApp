cc17e94952ff541b3a2a9b1ae2380aec8f66eebe
arminatwork <workwitharmin7@gmail.com>
2021-06-11 03:52:23 +0430

    refactor added crud operations signature

--- Pwa.Application.Contracts/Account/Role/IRoleApplication.cs
+++ Pwa.Application.Contracts/Account/Role/IRoleApplication.cs
@@ -1,8 +1,17 @@
-﻿using WebFramework.Infrastructure;
+﻿using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using WebFramework.Utilities;
 
 namespace Pwa.Application.Contracts.Account.Role
 {
     public interface IRoleApplication
     {
+        Task<List<RoleDto>> List();
+        Task<OperationResult> Create(CreateRoleDto dto, CancellationToken cancellationToken);
+        Task<OperationResult<EditRoleDto>> Get(int id, CancellationToken cancellationToken);
+        Task<OperationResult> Edit(EditRoleDto dto, CancellationToken cancellationToken);
+        Task<OperationResult<RoleDto>> Detail(int id, CancellationToken cancellationToken);
+        Task<OperationResult> Delete(int id, CancellationToken cancellationToken);
     }
 }
