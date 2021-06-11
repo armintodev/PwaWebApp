191e3e26f7a0a3bc13658ae3fb63e2d988eead98
arminatwork <workwitharmin7@gmail.com>
2021-06-11 20:21:07 +0430

    refactor configuration application cookie options to identity

--- Pwa.Infrastructure.EfCore/Extensions/IdentityExtension.cs
+++ Pwa.Infrastructure.EfCore/Extensions/IdentityExtension.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using System;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.DependencyInjection;
 using Pwa.Domain.Account;
 
@@ -8,15 +9,27 @@ namespace Pwa.Infrastructure.EfCore.Extensions
     {
         public static void AddIdentityApplication(this IServiceCollection service)
         {
-            service.AddIdentity<User, Role>(_ =>
-              {
-                  _.Password.RequireNonAlphanumeric = false;
-                  _.Password.RequireUppercase = false;
-                  _.SignIn.RequireConfirmedEmail = false;
-                  _.SignIn.RequireConfirmedPhoneNumber = true;
-                  _.SignIn.RequireConfirmedAccount = true;
-              }).AddEntityFrameworkStores<ApplicationDbContext>()
-                .AddDefaultTokenProviders();
+            service.AddIdentity<User, Role>()
+            .AddEntityFrameworkStores<ApplicationDbContext>();
+
+            service.ConfigureApplicationCookie(_ =>
+            {
+                _.Cookie.Name = "AuthApplication";
+                _.ExpireTimeSpan = TimeSpan.FromMinutes(20000);
+                _.LoginPath = "/user/login";
+                _.LogoutPath = "/user/logout";
+                _.AccessDeniedPath = "/user/accessdenied";
+                _.ClaimsIssuer = "DariaTechPwaArmin";
+            });
+
+            service.Configure<IdentityOptions>(_ =>
+            {
+                _.Password.RequireNonAlphanumeric = false;
+                _.Password.RequireUppercase = false;
+                _.SignIn.RequireConfirmedEmail = false;
+                _.SignIn.RequireConfirmedPhoneNumber = true;
+                _.SignIn.RequireConfirmedAccount = true;
+            });
         }
     }
 }
