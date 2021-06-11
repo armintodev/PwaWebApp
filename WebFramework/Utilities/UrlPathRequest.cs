b7076386721a924d0fd2f49043f2c36a1a4657cd
arminatwork <workwitharmin7@gmail.com>
2021-06-11 18:46:32 +0430

    refactor added user login, register, verify account url path request

--- WebFramework/Utilities/UrlPathRequest.cs
+++ WebFramework/Utilities/UrlPathRequest.cs
@@ -8,5 +8,9 @@
         public const string AdminCategory = "/Admin/Category";
         public const string AdminWebApp = "/Admin/WebApp";
         public const string AdminTicket = "/Admin/Ticket";
+
+        public const string UserLogin = "/User/Login";
+        public const string UserRegister = "/User/Register";
+        public const string UserVerifyAccount = "/User/VerifyAccount";
     }
 }
