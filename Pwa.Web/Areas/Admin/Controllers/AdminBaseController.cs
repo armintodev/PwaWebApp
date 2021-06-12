22019043ac2f98df4cce9306912ee550d9fa85fb
dariatech <daria@dariatech.ir>
2021-06-12 11:46:18 +0430

    feat added admin base controller

---
+++ Pwa.Web/Areas/Admin/Controllers/AdminBaseController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Pwa.Web.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize]
+    public class AdminBaseController : Controller
+    {
+    }
+}
