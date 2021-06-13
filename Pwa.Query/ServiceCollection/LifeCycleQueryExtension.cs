47954c73f31725be9cf868b3bd348af799bc8087
dariatech <daria@dariatech.ir>
2021-06-13 16:53:23 +0430

    feat added query life cycle extension

---
+++ Pwa.Query/ServiceCollection/LifeCycleQueryExtension.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Pwa.Query.Contracts.Category;
+using Pwa.Query.Contracts.WebApp;
+using Pwa.Query.Queries;
+
+namespace Pwa.Query.ServiceCollection
+{
+    public static class LifeCycleQueryExtension
+    {
+        public static void AddQueryService(this IServiceCollection service)
+        {
+            service.AddScoped<IWebAppQuery, WebAppQuery>();
+            service.AddScoped<ICategoryQuery, CategoryQuery>();
+        }
+    }
+}
