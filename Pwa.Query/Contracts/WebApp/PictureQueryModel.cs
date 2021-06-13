735ed71a1296710db8aba9661352072daaaccdd7
dariatech <daria@dariatech.ir>
2021-06-13 16:52:22 +0430

    feat added picture query model

---
+++ Pwa.Query/Contracts/WebApp/PictureQueryModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Pwa.Query.Contracts.WebApp
+{
+    public class PictureQueryModel
+    {
+        public int Id { get; set; }
+        public string Picture { get; set; }
+        public string Title { get; set; }
+        //public string Alt { get; set; }
+    }
+}
