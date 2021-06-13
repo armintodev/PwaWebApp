ba3ef5ee5a50160ae0f2dcc032ca7069f3098f8a
dariatech <daria@dariatech.ir>
2021-06-13 16:48:56 +0430

    refactor added web application icon path request

--- WebFramework/Utilities/Uploader/UploadPath.cs
+++ WebFramework/Utilities/Uploader/UploadPath.cs
@@ -4,5 +4,6 @@
     {
         public const string Developer = "Developer";
         public const string WebApplication = "WebApplication";
+        public const string WebApplicationIcon = "WebApplication/Icon";
     }
 }
