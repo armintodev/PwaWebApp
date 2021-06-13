00fecee555a0570a21925e8fdcdc9cc51aea9c06
dariatech <daria@dariatech.ir>
2021-06-13 16:51:36 +0430

    feat added category query interface signatures

---
+++ Pwa.Query/Contracts/Category/ICategoryQuery.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Pwa.Query.Contracts.Category
+{
+    public interface ICategoryQuery
+    {
+        Task<List<CategoryQueryModel>> List();
+        Task<List<CategoryQueryModel>> GetProductInCategory();
+    }
+}
