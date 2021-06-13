3bca80216f07d2f93033bbf2bb3f4b2b10243369
dariatech <daria@dariatech.ir>
2021-06-13 16:55:17 +0430

    feat added product in category view component,added this view

---
+++ Pwa.Web/ViewComponents/ProductInCategoryViewComponent.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Pwa.Query.Contracts.WebApp;
+using System.Threading.Tasks;
+
+namespace Pwa.Web.ViewComponents
+{
+    public class ProductInCategoryViewComponent : ViewComponent
+    {
+        private readonly IWebAppQuery _webApp;
+        public ProductInCategoryViewComponent(IWebAppQuery webApp)
+        {
+            _webApp = webApp;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View();
+        }
+    }
+}
