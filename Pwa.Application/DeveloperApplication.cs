d05c311805e733061cd7644e8fce7d0f6c663a77
arminatwork <workwitharmin7@gmail.com>
2021-06-13 20:57:13 +0430

    refactor clean coding

--- Pwa.Application/DeveloperApplication.cs
+++ Pwa.Application/DeveloperApplication.cs
@@ -22,17 +22,14 @@ namespace Pwa.Application
         private readonly IFileUploader _file;
         private readonly ISmsService _sms;
         private readonly UserManager<User> _userManager;
-        //private readonly SignInManager<Developer> _signInManager;
         public DeveloperApplication(IDeveloperRepository developer,
             UserManager<User> userManager,
-            //SignInManager<Developer> signInManager,
             IFileUploader file,
             ISmsService sms,
             IUserRepository user)
         {
             _userManager = userManager;
             _developer = developer;
-            // _signInManager = signInManager;
             _file = file;
             _sms = sms;
             _user = user;
@@ -71,7 +68,6 @@ namespace Pwa.Application
                 City = developer.City,
                 Province = developer.Province,
                 Country = developer.Country,
-                //ProfileUrl = developer.ProfileUrl,
             };
             return new OperationResult<EditDeveloperDto>(data, true, "");
         }
@@ -141,23 +137,6 @@ namespace Pwa.Application
             //var developer = await _developer.GetByIdAsync(CancellationToken.None, id);
             //developer.DeActive();
             await _developer.SaveChangesAsync();
-        }
-
-        public async Task<OperationResult> VerifyBySms(string code)
-        {
-            // get user
-            //var developer = await _userManager.GetUserAsync(_signInManager.Context.User);
-            //if (developer is null)
-            //    return new OperationResult(false, "توسعه دهنده ای برای تایید اس ام اس وجود ندارد");
-
-            ////check user code with parameter code
-            //if (developer.Code.Equals(code))
-            //{
-            //    await Activate(developer.Id);
-            //    await _developer.SaveChangesAsync();
-            //}
-
-            return new OperationResult();
         }
 
         public async Task<OperationResult<DeveloperDto>> Detail(int id)
