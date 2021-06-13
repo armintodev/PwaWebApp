e211e0be36e1c9ef35ab6cf07f2c1a36d45a1461
dariatech <daria@dariatech.ir>
2021-06-13 16:53:06 +0430

    feat implemented web app query

---
+++ Pwa.Query/Queries/WebAppQuery.cs
@@ -0,0 +1,30 @@
+﻿using Pwa.Infrastructure.EfCore;
+using Pwa.Query.Contracts.Category;
+using Pwa.Query.Contracts.WebApp;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Pwa.Query.Queries
+{
+    public class WebAppQuery : IWebAppQuery
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly ICategoryQuery _category;
+        public WebAppQuery(ApplicationDbContext context, ICategoryQuery category)
+        {
+            _context = context;
+            _category = category;
+        }
+
+        public async Task<List<WebAppQueryModel>> GetInCategory()
+        {
+            //var categories = await _category.List();
+            throw new System.NotImplementedException();
+        }
+
+        public Task<List<WebAppQueryModel>> GetMostVisited()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
