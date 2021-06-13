966f8f0fe8619316005962b9b8fbeb27b940b8e0
dariatech <daria@dariatech.ir>
2021-06-13 16:52:05 +0430

    feat added web app query interface signature

---
+++ Pwa.Query/Contracts/WebApp/IWebAppQuery.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Pwa.Query.Contracts.WebApp
+{
+    public interface IWebAppQuery
+    {
+        Task<List<WebAppQueryModel>> GetMostVisited();
+        Task<List<WebAppQueryModel>> GetInCategory();
+    }
+}
