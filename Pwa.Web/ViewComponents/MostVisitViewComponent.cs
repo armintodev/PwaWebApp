98c8914c44d0db1862d46445d96888335f621f73
arminatwork <workwitharmin7@gmail.com>
2021-06-16 00:46:00 +0430

    feat added most visit view component

---
+++ Pwa.Web/ViewComponents/MostVisitViewComponent.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Pwa.Query.Contracts.WebApp;
+using System.Threading.Tasks;
+
+namespace Pwa.Web.ViewComponents
+{
+    public class MostVisitViewComponent : ViewComponent
+    {
+        private readonly IWebAppQuery _webApp;
+        public MostVisitViewComponent(IWebAppQuery webApp)
+        {
+            _webApp = webApp;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var games = await _webApp.GetMostVisit();
+            return View(games);
+        }
+    }
+}
