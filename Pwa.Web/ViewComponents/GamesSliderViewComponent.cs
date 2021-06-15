1184bfa2acfd92a7f21ad76b6da46a01f6b1fe75
arminatwork <workwitharmin7@gmail.com>
2021-06-16 00:45:30 +0430

    feat added games slider view component

---
+++ Pwa.Web/ViewComponents/GamesSliderViewComponent.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Pwa.Query.Contracts.WebApp;
+using System.Threading.Tasks;
+
+namespace Pwa.Web.ViewComponents
+{
+    public class GamesSliderViewComponent : ViewComponent
+    {
+        private readonly IWebAppQuery _webApp;
+        public GamesSliderViewComponent(IWebAppQuery webApp)
+        {
+            _webApp = webApp;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var games = await _webApp.GetGames();
+            return View(games);
+        }
+    }
+}
