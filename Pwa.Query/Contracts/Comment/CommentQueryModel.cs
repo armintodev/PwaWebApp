1d2f9e361600137f047d28a18dfa1e340530bcb7
arminatwork <workwitharmin7@gmail.com>
2021-06-16 22:27:39 +0430

    refactor added missing properties

--- Pwa.Query/Contracts/Comment/CommentQueryModel.cs
+++ Pwa.Query/Contracts/Comment/CommentQueryModel.cs
@@ -2,5 +2,11 @@
 {
     public class CommentQueryModel
     {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public int Like { get; set; }
+        public string UserName { get; set; }
+        public string UserIcon { get; set; }
+        public string CreationDate { get; set; }
     }
 }
