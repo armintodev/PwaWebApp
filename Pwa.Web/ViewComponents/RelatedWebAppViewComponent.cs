a15bf97c609d097376ef053210c3cde523c2f6fd
arminatwork <workwitharmin7@gmail.com>
2021-06-16 22:50:55 +0430

    feat added related web app view component operation

---
+++ Pwa.Web/ViewComponents/RelatedWebAppViewComponent.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Pwa.Query.Contracts.WebApp;
+using System.Threading.Tasks;
+
+namespace Pwa.Web.ViewComponents
+{
+    public class RelatedWebAppViewComponent : ViewComponent
+    {
+        private readonly IWebAppQuery _webApp;
+        public RelatedWebAppViewComponent(IWebAppQuery webApp)
+        {
+            _webApp = webApp;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(int id)
+        {
+            var webApps = await _webApp.RelatedApps(id);
+            return View(webApps);
+        }
+    }
+}
