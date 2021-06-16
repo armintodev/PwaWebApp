b661242cc8d9ef5ae99f2f8d14bb9c5c50d2b9be
arminatwork <workwitharmin7@gmail.com>
2021-06-16 22:26:23 +0430

    refactor added create comment operations

--- Pwa.Application.Contracts/Product/Comment/ICommentApplication.cs
+++ Pwa.Application.Contracts/Product/Comment/ICommentApplication.cs
@@ -1,6 +1,11 @@
-﻿namespace Pwa.Application.Contracts.Product.Comment
+﻿using System.Threading;
+using System.Threading.Tasks;
+using WebFramework.Utilities;
+
+namespace Pwa.Application.Contracts.Product.Comment
 {
     public interface ICommentApplication
     {
+        Task<OperationResult> Add(CreateCommentDto dto, CancellationToken cancellationToken);
     }
 }
--- Pwa.Application/CommentApplication.cs
+++ Pwa.Application/CommentApplication.cs
@@ -1,14 +1,30 @@
-﻿using Pwa.Application.Contracts.Product.Comment;
+﻿using Microsoft.AspNetCore.Http;
+using Pwa.Application.Contracts.Product.Comment;
 using Pwa.Domain.Product;
+using System;
+using System.Security.Claims;
+using System.Threading;
+using System.Threading.Tasks;
+using WebFramework.Utilities;
 
 namespace Pwa.Application
 {
     public class CommentApplication : ICommentApplication
     {
         private readonly ICommentRepository _comment;
-        public CommentApplication(ICommentRepository comment)
+        private readonly IHttpContextAccessor _accessor;
+        public CommentApplication(ICommentRepository comment, IHttpContextAccessor accessor)
         {
             _comment = comment;
+            _accessor = accessor;
+        }
+
+        public async Task<OperationResult> Add(CreateCommentDto dto, CancellationToken cancellationToken)
+        {
+            var userId = Convert.ToInt32(_accessor.HttpContext.User.FindFirstValue(ClaimTypes.NameIdentifier));
+            Comment comment = new(dto.Description, userId, dto.WebApplicationId);
+            await _comment.AddAsync(comment, cancellationToken);
+            return new OperationResult();
         }
     }
 }
