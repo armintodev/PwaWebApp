cf63e30d93d8b4b6e927a1dfb0034ca5d7b02041
arminatwork <workwitharmin7@gmail.com>
2021-06-16 17:47:56 +0430

    refactor added invite friend operation with partial view

--- Pwa.Web/Controllers/HomeController.cs
+++ Pwa.Web/Controllers/HomeController.cs
@@ -1,12 +1,31 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using WebFramework.Utilities.Sms;
 
 namespace Pwa.Web.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly ISmsService _sms;
+        public HomeController(ISmsService sms)
+        {
+            _sms = sms;
+        }
+
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> InviteFriend(string phoneNumber)
+        {
+            if (string.IsNullOrWhiteSpace(phoneNumber))
+            {
+                return View();
+            }
+            await _sms.Send(phoneNumber, "شما به داریا وب اپ دعوت شده اید!");
+            return RedirectToAction("Index");
         }
     }
 }
