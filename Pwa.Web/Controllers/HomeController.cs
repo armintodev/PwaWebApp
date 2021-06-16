9d484f08077f0f8f578f07d71acf6d2e1a5151bd
arminatwork <workwitharmin7@gmail.com>
2021-06-16 22:50:10 +0430

    refactor implemented single web app view + action operations

--- Pwa.Web/Controllers/HomeController.cs
+++ Pwa.Web/Controllers/HomeController.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Pwa.Application.Contracts.Product.Comment;
+using Pwa.Query.Contracts.WebApp;
+using System.Threading;
 using System.Threading.Tasks;
 using WebFramework.Utilities.Sms;
 
@@ -6,10 +9,14 @@ namespace Pwa.Web.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly IWebAppQuery _webApp;
+        private readonly ICommentApplication _comment;
         private readonly ISmsService _sms;
-        public HomeController(ISmsService sms)
+        public HomeController(ISmsService sms, IWebAppQuery webApp, ICommentApplication comment)
         {
             _sms = sms;
+            _webApp = webApp;
+            _comment = comment;
         }
 
         public IActionResult Index()
@@ -26,6 +33,28 @@ namespace Pwa.Web.Controllers
             }
             await _sms.Send(phoneNumber, "شما به داریا وب اپ دعوت شده اید!");
             return RedirectToAction("Index");
+        }
+
+        public async Task<IActionResult> Single(int id)
+        {
+            var webApp = await _webApp.GetSingle(id);
+            webApp.Comment = new CreateCommentDto
+            {
+                WebApplicationId = webApp.Id
+            };
+            return View(webApp);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Single(CreateCommentDto comment, int id, CancellationToken cancellationToken)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await _comment.Add(comment, cancellationToken);
+                if (result.Success)
+                    return RedirectToAction("Single", new { Id = id });
+            }
+            return View(comment);
         }
     }
 }
