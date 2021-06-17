8866fc94c81cb4e0ce2887b361955548948f9207
dariatech <daria@dariatech.ir>
2021-06-17 14:45:48 +0430

    refactor implemented accepted, rejected, delete, detail, get, list operations signatures

--- Pwa.Application.Contracts/Product/Comment/ICommentApplication.cs
+++ Pwa.Application.Contracts/Product/Comment/ICommentApplication.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading;
+﻿using System.Collections.Generic;
+using System.Threading;
 using System.Threading.Tasks;
 using WebFramework.Utilities;
 
@@ -7,5 +8,11 @@ namespace Pwa.Application.Contracts.Product.Comment
     public interface ICommentApplication
     {
         Task<OperationResult> Add(CreateCommentDto dto, CancellationToken cancellationToken);
+        Task<OperationResult<EditCommentDto>> Get(int id, CancellationToken cancellationToken);
+        Task<List<CommentDto>> List(int id, CancellationToken cancellationToken);
+        Task<OperationResult> Delete(int id, CancellationToken cancellationToken);
+        Task<OperationResult<CommentDto>> Detail(int id, CancellationToken cancellationToken);
+        Task Accepted(int id, CancellationToken cancellationToken);
+        Task Rejected(int id, CancellationToken cancellationToken);
     }
 }
