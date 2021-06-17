5d7d9cd0892388710b9db6267a12c9114f374476
dariatech <daria@dariatech.ir>
2021-06-17 14:45:33 +0430

    refactor added display data annotation on properties, added missing property

--- Pwa.Application.Contracts/Product/Comment/CommentDto.cs
+++ Pwa.Application.Contracts/Product/Comment/CommentDto.cs
@@ -1,21 +1,33 @@
-﻿using WebFramework.Enums;
+﻿using System.ComponentModel.DataAnnotations;
+using WebFramework.Enums;
 using WebFramework.Infrastructure;
 
 namespace Pwa.Application.Contracts.Product.Comment
 {
     public record CommentDto : IDto
     {
+        [Display(Name = "آی دی")]
         public int Id { get; init; }
+
+        [Display(Name = "توضیحات")]
         public string Description { get; init; }
+
         public bool IsDeveloper { get; init; }
+
+        [Display(Name = "وضعیت")]
         public StatusDto Status { get; init; }
         public int UserId { get; init; }
         public int WebApplicationId { get; init; }
-        public string UserFullName { get; init; }
-        public string UserPhoneNumber { get; init; }
+
+        [Display(Name = "کاربر")]
+        public string UserInfo { get; init; }
+
+        [Display(Name = "وب اپلیکیشن")]
         public string WebAppName { get; init; }
+
+        [Display(Name = "آدرس")]
         public string WebAppAddress { get; init; }
 
-        //public WebApplicationDto WebApplication { get; init; }
+        public string CreationDate { get; init; }
     }
 }
--- Pwa.Application.Contracts/Product/Comment/EditCommentDto.cs
+++ Pwa.Application.Contracts/Product/Comment/EditCommentDto.cs
@@ -1,9 +1,11 @@
-﻿using WebFramework.Infrastructure;
+﻿using WebFramework.Enums;
+using WebFramework.Infrastructure;
 
 namespace Pwa.Application.Contracts.Product.Comment
 {
     public record EditCommentDto : CreateCommentDto, IDto
     {
         public int Id { get; init; }
+        public StatusDto Status { get; init; }
     }
 }
