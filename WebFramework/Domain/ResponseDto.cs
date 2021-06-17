a3298daf0081449db52ab4751bee56fe406e6edc
arminatwork <workwitharmin7@gmail.com>
2021-06-17 05:11:08 +0430

    feat added response dto

---
+++ WebFramework/Domain/ResponseDto.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace WebFramework.Domain
+{
+    public class ResponseDto<TData> where TData : class
+    {
+        public List<TData> Items { get; set; }
+        public Pager Pager { get; set; }
+        public int Page { get; set; }
+        public string Search { get; set; }
+    }
+}
