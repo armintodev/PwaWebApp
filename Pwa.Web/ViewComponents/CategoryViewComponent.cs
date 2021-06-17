dd047ed07bb5d28b5531381df1e5fdf09217eaae
arminatwork <workwitharmin7@gmail.com>
2021-06-17 05:10:02 +0430

    feat added category view component

---
+++ Pwa.Web/ViewComponents/CategoryViewComponent.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Pwa.Query.Contracts.Category;
+using System.Threading.Tasks;
+
+namespace Pwa.Web.ViewComponents
+{
+    public class CategoryViewComponent : ViewComponent
+    {
+        private readonly ICategoryQuery _category;
+        public CategoryViewComponent(ICategoryQuery category)
+        {
+            _category = category;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var categories = await _category.List();
+            return View(categories);
+        }
+    }
+}
