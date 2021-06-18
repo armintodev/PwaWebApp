9752a6fcca9f5d395cf2ac9bace43c367e3543b6
arminatwork <workwitharmin7@gmail.com>
2021-06-19 02:51:00 +0430

    feat added main info view component

---
+++ Pwa.Web/Areas/Admin/ViewComponents/MainViewComponent.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Pwa.Application.Contracts;
+using Pwa.Infrastructure.EfCore;
+using System.Threading.Tasks;
+
+namespace Pwa.Web.Areas.Admin.ViewComponents
+{
+    public class MainViewComponent : ViewComponent
+    {
+        private readonly ApplicationDbContext _context;
+        public MainViewComponent(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var counts = new MainDto
+            {
+                UserCount = await _context.Users.AsNoTracking().CountAsync(),
+                WebAppCount = await _context.WebApplications.AsNoTracking().CountAsync(),
+                CategoryCount = await _context.Categories.AsNoTracking().CountAsync()
+            };
+
+            return View(counts);
+        }
+    }
+}
