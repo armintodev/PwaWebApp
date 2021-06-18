a36265780d9b36c05a1f2e2b8e53ec3639a61f0b
arminatwork <workwitharmin7@gmail.com>
2021-06-18 19:14:15 +0430

    feat added user info view component, clean code

---
+++ Pwa.Web/ViewComponents/UserInfoViewComponent.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Pwa.Query.Contracts.User;
+using System.Threading.Tasks;
+
+namespace Pwa.Web.ViewComponents
+{
+    public class UserInfoViewComponent : ViewComponent
+    {
+        public readonly IUserQuery _user;
+        public UserInfoViewComponent(IUserQuery user)
+        {
+            _user = user;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var user = await _user.Info();
+            return View(user);
+        }
+    }
+}
