6d398afc85cd050488eb1cc08b0f9e65258694d2
arminatwork <workwitharmin7@gmail.com>
2021-06-19 02:08:04 +0430

    feat added sort order by constants

---
+++ WebFramework/Utilities/SortOrderBy.cs
@@ -0,0 +1,9 @@
+﻿namespace WebFramework.Utilities
+{
+    public class SortOrderBy
+    {
+        public const string MostInstalled = "most-installed";
+        public const string MostVisited = "most-visited";
+        public const string Newest = "newest";
+    }
+}
