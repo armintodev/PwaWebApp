bdc55e0609298015f1ea822e1fce9c68a8b06ddb
arminatwork <workwitharmin7@gmail.com>
2021-06-19 02:49:53 +0430

    feat added main dto to show in index page of admin panel

---
+++ Pwa.Application.Contracts/MainDto.cs
@@ -0,0 +1,12 @@
+﻿using WebFramework.Infrastructure;
+
+namespace Pwa.Application.Contracts
+{
+    public class MainDto : IDto
+    {
+        public int UserCount { get; init; }
+        public int WebAppCount { get; init; }
+        public int CategoryCount { get; init; }
+
+    }
+}
