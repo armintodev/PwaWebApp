bc55f764a97e4b2829c7d2a780b566dabe28b250
arminatwork <workwitharmin7@gmail.com>
2021-06-18 19:11:09 +0430

    feat added user query, implemented user info method

---
+++ Pwa.Query/Contracts/User/IUserQuery.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace Pwa.Query.Contracts.User
+{
+    public interface IUserQuery
+    {
+        Task<UserQueryModel> Info();
+    }
+}
---
+++ Pwa.Query/Queries/UserQuery.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Pwa.Domain.Account;
+using Pwa.Query.Contracts.User;
+using System.Threading.Tasks;
+
+namespace Pwa.Query.Queries
+{
+    public class UserQuery : IUserQuery
+    {
+        private readonly UserManager<User> _user;
+        private readonly IHttpContextAccessor _accessor;
+        public UserQuery(UserManager<User> user, IHttpContextAccessor accessor)
+        {
+            _user = user;
+            _accessor = accessor;
+        }
+
+        public async Task<UserQueryModel> Info()
+        {
+            var user = await _user.GetUserAsync(_accessor.HttpContext.User);
+            if (user is null)
+                return new UserQueryModel();
+            return new UserQueryModel
+            {
+                Name = user.UserName,
+                ProfileUrl = user.ProfileUrl
+            };
+        }
+    }
+}
