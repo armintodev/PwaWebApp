f201ce56e2ca269b2cb873f6c1948f054593aef4
arminatwork <workwitharmin7@gmail.com>
2021-06-19 02:50:36 +0430

    feat added admin info view component

---
+++ Pwa.Web/Areas/Admin/ViewComponents/AdminInfoViewComponent.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Pwa.Application.Contracts.Account.User;
+using System.Threading.Tasks;
+
+namespace Pwa.Web.Areas.Admin.ViewComponents
+{
+    public class AdminInfoViewComponent : ViewComponent
+    {
+        private readonly IUserApplication _user;
+        public AdminInfoViewComponent(IUserApplication user)
+        {
+            _user = user;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var admin = await _user.GetAdmin();
+            return View(admin);
+        }
+    }
+}
